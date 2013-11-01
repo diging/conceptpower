7673e2641d0f115b18aa9e7b4651da049d86b78b
prernasatija <prernasatija@03b148cf-524d-4926-b617-1f50de7337d5>
2013-11-01 21:03:21 +0000

    created viaf search .java file

---
+++ trunk/ConceptpowerPlus/src/edu/asu/conceptpower/viaf/ViafSearch.java
@@ -0,0 +1,30 @@
+package edu.asu.conceptpower.viaf;
+
+	import java.io.File;
+	import javax.xml.bind.JAXBContext;
+	import javax.xml.bind.JAXBException;
+	import javax.xml.bind.Unmarshaller;
+	 
+		
+	public class ViafSearch {
+		
+
+	public static void main(String[] args) {
+	 
+		 try {
+	 
+			File file = new File("C:\\file.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(ViafSearch.class);
+	 
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			ViafSearch search1 = (ViafSearch) jaxbUnmarshaller.unmarshal(file);
+			System.out.println(search1);
+	 
+		  } catch (JAXBException e) {
+			e.printStackTrace();
+		  }
+	 
+	}
+	}
+	
+
