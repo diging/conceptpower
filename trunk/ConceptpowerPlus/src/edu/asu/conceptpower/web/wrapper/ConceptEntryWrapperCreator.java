b6aa7d6978bdb4c2b89c022e526882eac3b85942
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2013-11-01 20:27:37 +0000

    added commits from GIT

--- trunk/ConceptpowerPlus/src/edu/asu/conceptpower/web/wrapper/ConceptEntryWrapperCreator.java
+++ trunk/ConceptpowerPlus/src/edu/asu/conceptpower/web/wrapper/ConceptEntryWrapperCreator.java
@@ -11,38 +11,70 @@ import edu.asu.conceptpower.users.UsersManager;
 
 public class ConceptEntryWrapperCreator {
 
-	public List<ConceptEntryWrapper> createWrappers(ConceptEntry[] entries, UsersManager usersManager, ConceptTypesManager typeManager, ConceptManager dictManager) {
+	public List<ConceptEntryWrapper> createWrappers(ConceptEntry[] entries,
+			UsersManager usersManager, ConceptTypesManager typeManager,
+			ConceptManager dictManager) {
 		List<ConceptEntryWrapper> foundConcepts = new ArrayList<ConceptEntryWrapper>();
-		
+
 		if (entries == null)
 			return foundConcepts;
-		
+
 		for (ConceptEntry entry : entries) {
 			ConceptEntryWrapper wrapper = new ConceptEntryWrapper(entry);
 			if (entry.getTypeId() != null && !entry.getTypeId().isEmpty())
 				wrapper.setType(typeManager.getType(entry.getTypeId()));
-			
+
 			if (entry.getCreatorId() != null && !entry.getCreatorId().isEmpty())
 				wrapper.setCreator(usersManager.findUser(entry.getCreatorId()));
-			
-			// if entry wraps a wordnet concept, add it here
-			if (entry.getWordnetId() != null && !entry.getWordnetId().trim().isEmpty()) {
-				if (!entry.getWordnetId().equals(entry.getId())) {
-					ConceptEntry wordnetEntry = dictManager.getWordnetConceptEntry(entry.getWordnetId());
-					wrapper.setWrappedWordnetEntry(wordnetEntry);
-					
-					//wrapper.setDescription(wrapper.getEntry().getDescription() + "\n\nWordnet description: \n" + wordnetEntry.getDescription());
-				}
-			}
-			
-			List<ConceptEntry> synonyms = new ArrayList<ConceptEntry>();
-			String synonymIds = (entry.getSynonymIds() != null ? entry.getSynonymIds() : ""); // + (wrapper.getWrappedWordnetEntry() != null ? Constants.SYNONYM_SEPARATOR + wrapper.getWrappedWordnetEntry().getSynonymIds() : "");
-			if (synonymIds != null) {
-				String[] ids = synonymIds.trim().split(Constants.SYNONYM_SEPARATOR);
+
+			// if entry wraps a wordnet concepts, add it here
+			List<ConceptEntry> wordnetEntries = new ArrayList<ConceptEntry>();
+			String wordnetIds = (entry.getWordnetId() != null ? entry
+					.getWordnetId() : "");
+			{
+				String[] ids = wordnetIds.trim().split(
+						Constants.CONCEPT_SEPARATOR);
 				if (ids != null) {
 					for (String id : ids) {
 						if (id != null && !id.trim().isEmpty()) {
-							ConceptEntry synonym = dictManager.getConceptEntry(id);
+							ConceptEntry wordnetEntry = dictManager
+									.getWordnetConceptEntry(id);
+							if (wordnetEntry != null)
+								wordnetEntries.add(wordnetEntry);
+						}
+					}
+					wrapper.setWrappedWordnetEntries(wordnetEntries);
+
+				}
+			}
+
+			// build description considering all the wordnet entries wrappe
+			StringBuffer sb = new StringBuffer();
+			if (wordnetEntries.size() > 0) {
+				sb.append("Wordnet description:<br/>");
+				for (ConceptEntry wordnetConcept : wordnetEntries) {
+					sb.append("<br></br>");
+					if (wordnetConcept.getDescription() != null) {
+						sb.append("<i>" + wordnetConcept.getWord()
+								+ "</i><br/>");
+						sb.append(wordnetConcept.getDescription());
+					}
+				}
+			}
+			wrapper.setDescription(sb.toString());
+
+			List<ConceptEntry> synonyms = new ArrayList<ConceptEntry>();
+			String synonymIds = (entry.getSynonymIds() != null ? entry
+					.getSynonymIds() : "");
+			if (synonymIds != null) {
+
+				String[] ids = synonymIds.trim().split(
+						Constants.SYNONYM_SEPARATOR);
+				if (ids != null) {
+					for (String id : ids) {
+						if (id != null && !id.trim().isEmpty()) {
+							ConceptEntry synonym = dictManager
+									.getConceptEntry(id);
 							if (synonym != null)
 								synonyms.add(synonym);
 						}
@@ -52,7 +84,7 @@ public class ConceptEntryWrapperCreator {
 			}
 			foundConcepts.add(wrapper);
 		}
-		
+
 		return foundConcepts;
 	}
 }
