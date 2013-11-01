5d4d667a040cd42bfb9587f2521ab415cf9526d3
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2013-11-01 20:10:49 +0000

    commited concept seperator

--- trunk/ConceptpowerPlus/src/edu/asu/conceptpower/wordnet/Constants.java
+++ trunk/ConceptpowerPlus/src/edu/asu/conceptpower/wordnet/Constants.java
@@ -3,4 +3,5 @@ package edu.asu.conceptpower.wordnet;
 public interface Constants {
 
 	public final static String WORDNET_DICTIONARY = "WordNet";
+	public final static String CONCEPT_SEPARATOR = ",";
 }
