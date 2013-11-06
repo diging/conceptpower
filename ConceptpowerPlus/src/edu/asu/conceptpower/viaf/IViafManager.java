f13e02ff0173200a191948a1cecf01d93dc2abee
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2013-11-06 22:32:40 +0000

    commented the code which had compiler errors

--- ConceptpowerPlus/src/edu/asu/conceptpower/viaf/IViafManager.java
+++ ConceptpowerPlus/src/edu/asu/conceptpower/viaf/IViafManager.java
@@ -1,13 +1,5 @@
 package edu.asu.conceptpower.viaf;
-import java.util.List;
 
-	import java.util.List;
-
-	import edu.asu.viaf.ViafReply;
-
-
-	public interface IViafManager {
-		public List<ViafReply.Items> search(String item, int startIndex);
-	}
-
-
+public interface IViafManager {
+	// public List<ViafReply.Items> search(String item, int startIndex);
+}
