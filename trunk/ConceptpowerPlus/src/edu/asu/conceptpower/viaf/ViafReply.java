0c89ebc36ad8a9190a1412331916004e72220426
prernasatija <prernasatija@03b148cf-524d-4926-b617-1f50de7337d5>
2013-11-07 17:45:16 +0000

    Prerna- modified the parse xml code

--- trunk/ConceptpowerPlus/src/edu/asu/conceptpower/viaf/ViafReply.java
+++ trunk/ConceptpowerPlus/src/edu/asu/conceptpower/viaf/ViafReply.java
@@ -1,8 +1,6 @@
-package edu.asu.conceptpower.viaf;
+package edu.asu.conceptpower.viaf;	
 	
-	
-	import java.util.List;
-
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -10,22 +8,62 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = { "items"})
+@XmlRootElement(name="rss")
+public class ViafReply {
+	
+	@XmlElementWrapper(name="channel")
+	@XmlElement(name="item")
+	private List<ViafReply.Items> items;
+	
+	public List<ViafReply.Items> getItems() {
+		return items;
+	}
+	public void setItems(List<ViafReply.Items> items) {
+		this.items = items;
+	}
+
+
 	@XmlAccessorType(XmlAccessType.FIELD)
-	@XmlType(propOrder = { "items"})
-	@XmlRootElement(name="rss")
-	public class ViafReply {
-		/*	
-		@XmlElementWrapper(name="channel")
-		@XmlElement(name="item")
-		private List<ViafReply.Items> items;
+	@XmlType(propOrder={"title","link","pubDate","guid"})
+	public static class Items {
 		
-		public List<ViafReply.Items> getItems() {
-			return items;
+		@XmlElement(required=true)
+		private String title;
+		@XmlElement(required=true)
+		private String link;
+		@XmlElement(required=true)
+		private String pubDate;
+		@XmlElement(required=true)
+		private String guid;
+		
+		public String getTitle() {
+			return title;
 		}
-		public void setItems(List<ViafReply.Items> items) {
-			this.items = items;
-		}*/
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getLink() {
+			return link;
+		}
+		public void setLink(String link) {
+			this.link = link;
+		}
+		public String getPubDate() {
+			return pubDate;
+		}
+		public void setPubDate(String pubDate) {
+			this.pubDate = pubDate;
+		}
+		public String getGuid() {
+			return guid;
+		}
+		public void setGuid(String guid) {
+			this.guid = guid;
+		}
+	}
+
+}	
 	
 	
-	
-}
