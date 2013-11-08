ce53bf75b3ead034e2330b29551a1fe3ee447a1b
prernasatija <prernasatija@03b148cf-524d-4926-b617-1f50de7337d5>
2013-11-08 22:57:40 +0000

    CReated new java files for calling api and getting search result

--- trunk/ConceptpowerPlus/src/edu/asu/conceptpower/viaf/ViafManager.java
+++ trunk/ConceptpowerPlus/src/edu/asu/conceptpower/viaf/ViafManager.java
@@ -2,40 +2,6 @@ package edu.asu.conceptpower.viaf;
 
 import java.util.List;
 
-//@Service
 public class ViafManager {
-/*	@Autowired
-	@Qualifier("restTemplate")
-	RestTemplate restTemplate;
-
-	@Autowired
-	@Qualifier("viafURL")
-	private String viafURL;
-
-	@Autowired
-	@Qualifier("searchViafURLPath")
-	private String searchViafURLPath;
-
-	@Autowired
-	@Qualifier("searchViafURLPath1")
-	private String searchViafURLPath1;
-
-	@Autowired
-	@Qualifier("searchViafURLPath2")
-	private String searchViafURLPath2;
-
-	@Override
-	public List<ViafReply.Items> search(String item, int startIndex) {
-
-		List<ViafReply.Items> items = null;
-
-		String fullUrl = viafURL + searchViafURLPath + item
-				+ searchViafURLPath1 + startIndex + searchViafURLPath2;
-		ViafReply rep = (ViafReply) restTemplate.getForObject(fullUrl,
-				ViafReply.class);
-		items = rep.getItems();
-
-		return items;
-	}
-*/
+	
 }
