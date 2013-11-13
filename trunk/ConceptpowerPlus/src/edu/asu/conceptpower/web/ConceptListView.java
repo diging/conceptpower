d7073eee561b9d5bb02cde4021cb4fb02bf6e528
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2013-11-13 23:27:45 +0000

    Filters for concept term and description

--- trunk/ConceptpowerPlus/src/edu/asu/conceptpower/web/ConceptListView.java
+++ trunk/ConceptpowerPlus/src/edu/asu/conceptpower/web/ConceptListView.java
@@ -32,17 +32,19 @@ public class ConceptListView extends DatabaseBean implements Serializable {
 
 	@ManagedProperty("#{userSuperAdminController}")
 	private UserSuperAdminController adminController;
-	
+
 	@ManagedProperty("#{wordNetConfController}")
 	private WordNetConfController configurationController;
-	
+
 	private String conceptIdForDelete;
-	
+
 	private int page;
 	private String selectedConceptForEdit;
 	private ConceptEntryWrapper selectedConceptForShow;
 	private String selectedConceptIdForShow;
-	
+	private String filterTerm;
+	private String filterDescription;
+
 	public UserSuperAdminController getAdminController() {
 		return adminController;
 	}
@@ -55,89 +57,101 @@ public class ConceptListView extends DatabaseBean implements Serializable {
 	private List<ConceptEntryWrapper> entries;
 	private transient ListDataModel<ConceptEntryWrapper> datamodel;
 
-
 	@PostConstruct
 	public void init() {
-		//conceptList = listManager.getSelectedList();
+		// conceptList = listManager.getSelectedList();
 		conceptList = null;
 		DatabaseController controller = getDatabaseController();
-		
+
 		DatabaseManager manager = controller.getDatabaseProvider()
-		.getDatabaseManager(DBNames.WORDNET_CACHE);
+				.getDatabaseManager(DBNames.WORDNET_CACHE);
 
 		ConceptManager conceptManager;
 		try {
-			conceptManager = new ConceptManager(manager, controller.getDatabaseProvider().getDatabaseManager(DBNames.DICTIONARY_DB),
+			conceptManager = new ConceptManager(manager, controller
+					.getDatabaseProvider().getDatabaseManager(
+							DBNames.DICTIONARY_DB),
 					configurationController.getWordNetConfiguration());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return;
 		}
-		
+
 		List<ConceptEntry> founds = null;
 		if (conceptList != null)
-			founds = conceptManager.getConceptListEntries(conceptList.getConceptListName());
+			founds = conceptManager.getConceptListEntries(conceptList
+					.getConceptListName());
 		else if (FacesUtil.getSessionMapValue(Parameter.SELECTED_LIST) != null) {
-			Object selectedListObj = FacesUtil.getSessionMapValue(Parameter.SELECTED_LIST);
-			
+			Object selectedListObj = FacesUtil
+					.getSessionMapValue(Parameter.SELECTED_LIST);
+
 			if (selectedListObj instanceof ConceptList)
-				founds = conceptManager.getConceptListEntries(((ConceptList)selectedListObj).getConceptListName());
-			else
-			{
-				founds = conceptManager.getConceptListEntries((String) selectedListObj);				
+				founds = conceptManager
+						.getConceptListEntries(((ConceptList) selectedListObj)
+								.getConceptListName());
+			else {
+				founds = conceptManager
+						.getConceptListEntries((String) selectedListObj);
 			}
 		}
-		
+
 		ConceptTypesManager typeManager;
 		try {
-			typeManager = new ConceptTypesManager(controller.getDatabaseProvider().getDatabaseManager(DBNames.TYPES_DB).getClient());
+			typeManager = new ConceptTypesManager(controller
+					.getDatabaseProvider().getDatabaseManager(DBNames.TYPES_DB)
+					.getClient());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return;
 		}
-		
-		DatabaseManager userDBManager = controller.getDatabaseProvider().getDatabaseManager(DBNames.USER_DB);
+
+		DatabaseManager userDBManager = controller.getDatabaseProvider()
+				.getDatabaseManager(DBNames.USER_DB);
 
 		UsersManager usersManager = new UsersManager(userDBManager);
 		usersManager.setAdmins(adminController.getAdmins());
-		
+
 		ConceptEntryWrapperCreator wrapperCreator = new ConceptEntryWrapperCreator();
-		entries = wrapperCreator.createWrappers(founds != null ? founds.toArray(new ConceptEntry[founds.size()]) : new ConceptEntry[0], usersManager, typeManager, conceptManager);
-		
+		entries = wrapperCreator
+				.createWrappers(
+						founds != null ? founds.toArray(new ConceptEntry[founds
+								.size()]) : new ConceptEntry[0], usersManager,
+						typeManager, conceptManager);
+
 		conceptManager.close();
 		typeManager.close();
 		usersManager.close();
-		
-		
+
 	}
-	
+
 	public String selectForEdit() {
 		for (ConceptEntryWrapper w : entries) {
 			if (w.getEntry().getId().equals(selectedConceptForEdit)) {
 				ConceptEntry selected = w.getEntry();
-				FacesUtil.setSessionMapValue(Parameter.SELECTED_CONCEPT, selected);			
+				FacesUtil.setSessionMapValue(Parameter.SELECTED_CONCEPT,
+						selected);
 			}
 		}
-		
+
 		return "editConcept";
 	}
-	
+
 	public String selectForShowDetails() {
 		for (ConceptEntryWrapper w : entries) {
 			if (w.getEntry().getId().equals(selectedConceptIdForShow)) {
-				FacesUtil.setSessionMapValue(Parameter.SELECTED_CONCEPT, w);	
+				FacesUtil.setSessionMapValue(Parameter.SELECTED_CONCEPT, w);
 			}
 		}
-		
+
 		return "showConceptDetails";
 	}
-	
+
 	public String selectForDelete() {
 		for (ConceptEntryWrapper w : entries) {
 			if (w.getEntry().getId().equals(conceptIdForDelete)) {
-				FacesUtil.setSessionMapValue(Parameter.DELETE_CONCEPT, w);	
+				FacesUtil.setSessionMapValue(Parameter.DELETE_CONCEPT, w);
 			}
 		}
 		return "deleteConcept";
@@ -150,8 +164,9 @@ public class ConceptListView extends DatabaseBean implements Serializable {
 	public ConceptList getConceptList() {
 		return conceptList;
 	}
-	
-	public void setConfigurationController(WordNetConfController configurationController) {
+
+	public void setConfigurationController(
+			WordNetConfController configurationController) {
 		this.configurationController = configurationController;
 	}
 
@@ -183,14 +198,15 @@ public class ConceptListView extends DatabaseBean implements Serializable {
 		return selectedConceptForEdit;
 	}
 
-	public void setSelectedConceptForShow(ConceptEntryWrapper selectedConceptForShow) {
+	public void setSelectedConceptForShow(
+			ConceptEntryWrapper selectedConceptForShow) {
 		this.selectedConceptForShow = selectedConceptForShow;
 	}
 
 	public ConceptEntryWrapper getSelectedConceptForShow() {
 		return selectedConceptForShow;
 	}
-	
+
 	public String selectForShow() {
 		for (ConceptEntryWrapper w : entries) {
 			if (w.getEntry().getId().equals(selectedConceptIdForShow)) {
@@ -198,7 +214,7 @@ public class ConceptListView extends DatabaseBean implements Serializable {
 				return "";
 			}
 		}
-		
+
 		return "";
 	}
 
@@ -220,11 +236,28 @@ public class ConceptListView extends DatabaseBean implements Serializable {
 
 	public ListDataModel<ConceptEntryWrapper> getDatamodel() {
 		datamodel = new ListDataModel<ConceptEntryWrapper>();
-		((ListDataModel<ConceptEntryWrapper>)datamodel).setWrappedData(entries);
+		((ListDataModel<ConceptEntryWrapper>) datamodel)
+				.setWrappedData(entries);
 		return datamodel;
 	}
 
 	public void setDatamodel(ListDataModel<ConceptEntryWrapper> datamodel) {
 		this.datamodel = datamodel;
+	}
+
+	public String getFilterTerm() {
+		return filterTerm;
+	}
+
+	public void setFilterTerm(String filterTerm) {
+		this.filterTerm = filterTerm;
+	}
+
+	public String getFilterDescription() {
+		return filterDescription;
+	}
+
+	public void setFilterDescription(String filterDescription) {
+		this.filterDescription = filterDescription;
 	}
 }
