04650dfef94f9686789017311eed281ece8384e6
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2013-11-19 23:02:28 +0000

    Added Wrapper Classes for Concept Wrapper for wordnet concepts

---
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/wrapper/ConceptEntryWrapper.java
@@ -0,0 +1,89 @@
+package edu.asu.conceptpower.web.wrapper;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.asu.conceptpower.core.ConceptEntry;
+import edu.asu.conceptpower.core.ConceptType;
+import edu.asu.conceptpower.users.User;
+import edu.asu.conceptpower.util.URICreator;
+
+public class ConceptEntryWrapper implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4261304897583134670L;
+	private ConceptEntry entry;
+	private List<ConceptEntry> synonyms;
+	private ConceptType type;
+	private User creator;
+	private List<ConceptEntry> wrappedWordnetEntries;
+	private String description;
+
+	@Autowired
+	URICreator URICreator;
+
+	public ConceptEntryWrapper(ConceptEntry entry) {
+		this.entry = entry;
+	}
+
+	public ConceptEntry getEntry() {
+		return entry;
+	}
+
+	public void setEntry(ConceptEntry entry) {
+		this.entry = entry;
+	}
+
+	public List<ConceptEntry> getSynonyms() {
+		return synonyms;
+	}
+
+	public void setSynonyms(List<ConceptEntry> synonyms) {
+		this.synonyms = synonyms;
+	}
+
+	public ConceptType getType() {
+		return type;
+	}
+
+	public void setType(ConceptType type) {
+		this.type = type;
+	}
+
+	public User getCreator() {
+		return creator;
+	}
+
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+
+	public void setWrappedWordnetEntries(
+			List<ConceptEntry> wrappedWordnetEntries) {
+		this.wrappedWordnetEntries = wrappedWordnetEntries;
+	}
+
+	public List<ConceptEntry> getWrappedWordnetEntries() {
+		return wrappedWordnetEntries;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description.replace("\n", "<br/>");
+	}
+
+	public String getUri() {
+		if (entry == null)
+			return "";
+
+		return URICreator.getURI(entry);
+	}
+
+}
---
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/wrapper/ConceptEntryWrapperCreator.java
@@ -0,0 +1,90 @@
+package edu.asu.conceptpower.web.wrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.asu.conceptpower.core.ConceptEntry;
+import edu.asu.conceptpower.core.ConceptManager;
+import edu.asu.conceptpower.core.ConceptTypesManager;
+import edu.asu.conceptpower.core.Constants;
+import edu.asu.conceptpower.users.impl.UsersManager;
+
+public class ConceptEntryWrapperCreator {
+
+	public List<ConceptEntryWrapper> createWrappers(ConceptEntry[] entries,
+			UsersManager usersManager, ConceptTypesManager typeManager,
+			ConceptManager dictManager) {
+		List<ConceptEntryWrapper> foundConcepts = new ArrayList<ConceptEntryWrapper>();
+
+		if (entries == null)
+			return foundConcepts;
+
+		for (ConceptEntry entry : entries) {
+			ConceptEntryWrapper wrapper = new ConceptEntryWrapper(entry);
+			if (entry.getTypeId() != null && !entry.getTypeId().isEmpty())
+				wrapper.setType(typeManager.getType(entry.getTypeId()));
+
+			if (entry.getCreatorId() != null && !entry.getCreatorId().isEmpty())
+				wrapper.setCreator(usersManager.findUser(entry.getCreatorId()));
+
+			// if entry wraps a wordnet concepts, add it here
+			List<ConceptEntry> wordnetEntries = new ArrayList<ConceptEntry>();
+			String wordnetIds = (entry.getWordnetId() != null ? entry
+					.getWordnetId() : "");
+			{
+				String[] ids = wordnetIds.trim().split(
+						Constants.CONCEPT_SEPARATOR);
+				if (ids != null) {
+					for (String id : ids) {
+						if (id != null && !id.trim().isEmpty()) {
+							ConceptEntry wordnetEntry = dictManager
+									.getWordnetConceptEntry(id);
+							if (wordnetEntry != null)
+								wordnetEntries.add(wordnetEntry);
+						}
+					}
+					wrapper.setWrappedWordnetEntries(wordnetEntries);
+
+				}
+			}
+
+			// build description considering all the wordnet entries wrappe
+			StringBuffer sb = new StringBuffer();
+			if (wordnetEntries.size() > 0) {
+				sb.append("Wordnet description:<br/>");
+				for (ConceptEntry wordnetConcept : wordnetEntries) {
+					sb.append("<br></br>");
+					if (wordnetConcept.getDescription() != null) {
+						sb.append("<i>" + wordnetConcept.getWord()
+								+ "</i><br/>");
+						sb.append(wordnetConcept.getDescription());
+					}
+				}
+			}
+			wrapper.setDescription(sb.toString());
+
+			List<ConceptEntry> synonyms = new ArrayList<ConceptEntry>();
+			String synonymIds = (entry.getSynonymIds() != null ? entry
+					.getSynonymIds() : "");
+			if (synonymIds != null) {
+
+				String[] ids = synonymIds.trim().split(
+						Constants.SYNONYM_SEPARATOR);
+				if (ids != null) {
+					for (String id : ids) {
+						if (id != null && !id.trim().isEmpty()) {
+							ConceptEntry synonym = dictManager
+									.getConceptEntry(id);
+							if (synonym != null)
+								synonyms.add(synonym);
+						}
+					}
+					wrapper.setSynonyms(synonyms);
+				}
+			}
+			foundConcepts.add(wrapper);
+		}
+
+		return foundConcepts;
+	}
+}
