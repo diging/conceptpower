312dab515fac8ccd3d384febfc5e7ec04b505e24
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2013-12-08 01:06:51 +0000

    added controller annotation

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/core/ConceptTypesManager.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/core/ConceptTypesManager.java
@@ -3,16 +3,18 @@ package edu.asu.conceptpower.core;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import edu.asu.conceptpower.db4o.TypeDatabaseClient;
 
+@Controller
 public class ConceptTypesManager {
 
 	@Autowired
 	private TypeDatabaseClient client;
-	
+
 	public void addConceptType(ConceptType type) {
-		
+
 		String id = null;
 		while (true) {
 			id = UUID.randomUUID().toString();
@@ -20,22 +22,22 @@ public class ConceptTypesManager {
 			if (exist == null)
 				break;
 		}
-		
+
 		type.setTypeId(id);
-		
+
 		client.addType(type);
 	}
-	
+
 	public void storeModifiedConceptType(ConceptType type) {
 		client.update(type);
 	}
-	
+
 	public ConceptType[] getAllTypes() {
 		return client.getAllTypes();
 	}
-	
+
 	public ConceptType getType(String id) {
 		return client.getType(id);
 	}
-	
+
 }
