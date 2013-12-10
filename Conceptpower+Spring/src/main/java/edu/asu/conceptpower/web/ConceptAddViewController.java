922d39ff2b9b412b33aab4ba1b707b76ce0c8a08
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2013-12-10 00:48:08 +0000

    implemented concept creation method in ConceptAddViewController and added JSP page to view the list of existing concepts under a list

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/ConceptAddViewController.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/ConceptAddViewController.java
@@ -1,18 +1,98 @@
 package edu.asu.conceptpower.web;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.asu.conceptpower.core.ConceptEntry;
+import edu.asu.conceptpower.core.ConceptList;
+import edu.asu.conceptpower.core.ConceptManager;
+import edu.asu.conceptpower.core.ConceptType;
+import edu.asu.conceptpower.core.ConceptTypesManager;
+import edu.asu.conceptpower.exceptions.DictionaryDoesNotExistException;
+import edu.asu.conceptpower.exceptions.DictionaryModifyException;
+import edu.asu.conceptpower.wrapper.ConceptEntryWrapper;
+import edu.asu.conceptpower.wrapper.ConceptEntryWrapperCreator;
 
 @Controller
 public class ConceptAddViewController {
 
+	@Autowired
+	private ConceptManager conceptManager;
+
+	@Autowired
+	private ConceptListManager conceptListManager;
+
+	@Autowired
+	ConceptEntryWrapperCreator wrapperCreator;
+
+	@Autowired
+	private ConceptTypesManager conceptTypesManager;
+
+	private List<ConceptList> allLists;
+	private ConceptType[] allTypes;
+	private ConceptEntry conceptEntry;
+
 	@RequestMapping(value = "auth/concepts/ConceptAddView")
-	public String search(HttpServletRequest req, ModelMap model) {
+	public String conceptAddView(HttpServletRequest req, ModelMap model) {
+
+		allTypes = conceptTypesManager.getAllTypes();
+		Map<String, String> types = new LinkedHashMap<String, String>();
+		for (ConceptType conceptType : allTypes) {
+			types.put(conceptType.getTypeId(), conceptType.getTypeName());
+		}
+
+		model.addAttribute("types", types);
+
+		allLists = conceptManager.getAllConceptLists();
+		Map<String, String> lists = new LinkedHashMap<String, String>();
+		for (ConceptList conceptList : allLists) {
+			lists.put(conceptList.getConceptListName(),
+					conceptList.getConceptListName());
+		}
+		model.addAttribute("lists", lists);
 
 		return "/auth/concepts/ConceptAddView";
 	}
 
+	@RequestMapping(value = "auth/concepts/createconcept", method = RequestMethod.POST)
+	public String createConcept(HttpServletRequest req, ModelMap model) {
+
+		try {
+			conceptEntry = new ConceptEntry();
+			conceptEntry.setWord(req.getParameter("name"));
+			conceptEntry.setConceptList(req.getParameter("lists"));
+			conceptEntry.setPos(req.getParameter("pos"));
+			conceptEntry.setDescription(req.getParameter("description"));
+			conceptEntry.setEqualTo(req.getParameter("equals"));
+			conceptEntry.setSimilarTo(req.getParameter("similar"));
+			conceptManager.addConceptListEntry(conceptEntry);
+
+		} catch (DictionaryDoesNotExistException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "failed";
+		} catch (DictionaryModifyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "failed";
+		}
+
+		List<ConceptEntry> found = conceptManager.getConceptListEntries(req
+				.getParameter("lists"));
+
+		List<ConceptEntryWrapper> foundConcepts = wrapperCreator
+				.createWrappers((ConceptEntry[]) found.toArray());
+
+		model.addAttribute("result", foundConcepts);
+		return "/auth/concepts/ConceptListView";
+	}
 }
--- Conceptpower+Spring/src/main/webapp/WEB-INF/views/auth/concepts/ConceptListView.jsp
+++ Conceptpower+Spring/src/main/webapp/WEB-INF/views/auth/concepts/ConceptListView.jsp
@@ -2,39 +2,40 @@
 <%@ taglib prefix="sec"
 	uri="http://www.springframework.org/security/tags"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <%@ page session="false"%>
-
-
-<script type="text/javascript">
-	$(document).ready(function() {
-		$('#conceptList').dataTable({
-			"bJQueryUI" : true,
-			"sPaginationType" : "full_numbers",
-			"bAutoWidth" : false
-		});
-
-	});
-</script>
-
-<h1>Concept Lists</h1>
-<p>Here you find all stored concept lists.</p>
 
 <c:if test="${not empty result}">
 	<table cellpadding="0" cellspacing="0" class="display dataTable"
-		id="conceptList">
+		id="conceptSearch">
 		<thead>
 			<tr>
-				<th>Name</th>
+				<th>Term</th>
+				<th>ID</th>
+				<th>Wordnet ID</th>
+				<th>POS</th>
+				<th>Concept List</th>
 				<th>Description</th>
+				<th>Type</th>
 			</tr>
 		</thead>
 		<tbody>
-			<c:forEach var="list" items="${result}">
+			<c:forEach var="concept" items="${result}">
 				<tr class="gradeX">
 					<td align="justify"><font size="2"><c:out
-								value="${list.conceptListName}"></c:out></font></td>
+								value="${concept.entry.word}"></c:out></font></td>
 					<td align="justify"><font size="2"><c:out
-								value="${list.description}"></c:out></font></td>
+								value="${concept.entry.id}"></c:out></font></td>
+					<td align="justify"><font size="2"><c:out
+								value="${concept.entry.wordnetId}"></c:out></font></td>
+					<td align="justify"><font size="2"><c:out
+								value="${concept.entry.pos}"></c:out></font></td>
+					<td align="justify"><font size="2"><c:out
+								value="${concept.entry.conceptList}"></c:out></font></td>
+					<td align="justify"><font size="2"><c:out
+								value="${concept.description}"></c:out></font></td>
+					<td align="justify"><font size="2"><c:out
+								value="${concept.type.typeName}"></c:out></font></td>
 				</tr>
 			</c:forEach>
 		</tbody>
