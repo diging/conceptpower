6ac44155b235435383f30d040a9e5b95d7ff6bfb
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2014-02-18 00:36:12 +0000

    added viaf search

--- trunk/Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/ConceptSearchController.java
+++ trunk/Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/ConceptSearchController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.asu.conceptpower.core.ConceptEntry;
 import edu.asu.conceptpower.core.ConceptManager;
+import edu.asu.conceptpower.profile.ISearchResult;
+import edu.asu.conceptpower.profile.impl.ViafService;
 import edu.asu.conceptpower.wrapper.ConceptEntryWrapper;
 import edu.asu.conceptpower.wrapper.ConceptEntryWrapperCreator;
 
@@ -24,15 +26,23 @@ public class ConceptSearchController {
 	@Autowired
 	ConceptEntryWrapperCreator wrapperCreator;
 
+	@Autowired
+	ViafService viafService;
+
 	private String concept;
 	private List<ConceptEntryWrapper> foundConcepts;
 	private String pos;
+	private String searchengine;
 
 	@RequestMapping(value = "/home/conceptsearch", method = RequestMethod.POST)
 	public String search(HttpServletRequest req, ModelMap model) {
 
 		concept = req.getParameter("name");
 		pos = req.getParameter("pos");
+		searchengine = req.getParameter("searchengine");
+
+		List<ISearchResult> viafResult = viafService.search(concept);
+
 		if (!concept.trim().isEmpty()) {
 
 			ConceptEntry[] found = conceptManager.getConceptListEntriesForWord(
