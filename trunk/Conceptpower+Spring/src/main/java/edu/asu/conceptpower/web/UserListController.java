fb5850c804d8f6c37207153b446173c3a940d2aa
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2014-03-30 23:46:02 +0000

    updated

--- trunk/Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/UserListController.java
+++ trunk/Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/UserListController.java
@@ -8,20 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import edu.asu.conceptpower.users.IUserManager;
 import edu.asu.conceptpower.users.User;
 
-
 @Controller
 public class UserListController {
 
 	@Autowired
 	private IUserManager userManager;
-	
+
 	@RequestMapping(value = "auth/user/list")
 	public String listUsers(ModelMap model) {
 		User[] users = userManager.getAllUsers();
 		model.addAttribute("users", users);
-		
+
 		return "auth/user/list";
 	}
-
 
 }
