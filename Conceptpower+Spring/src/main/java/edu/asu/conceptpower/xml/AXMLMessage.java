27741125ba7ba6c397ab4b3c55c506d01f97216b
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2014-04-08 00:44:31 +0000

    removed unnecessary methods

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/xml/AXMLMessage.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/xml/AXMLMessage.java
@@ -1,35 +1,23 @@
 package edu.asu.conceptpower.xml;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AXMLMessage {
-	
-	private List<String> entries;
 
-	public AXMLMessage() {
-		super();
-		entries = new ArrayList<String>();
-	}
-
-	public String getXML() {
+	public String getXML(List<String> entries) {
 		StringBuffer sb = new StringBuffer();
-	
+
 		sb.append("<" + XMLConstants.CONCEPTPOWER_ANSWER + " xmlns:"
 				+ XMLConstants.NAMESPACE_PREFIX + "=\""
 				+ XMLConstants.NAMESPACE + "\">");
-	
+
 		for (String entry : entries) {
 			sb.append(entry);
 		}
-	
+
 		sb.append("</" + XMLConstants.CONCEPTPOWER_ANSWER + ">");
-	
+
 		return sb.toString();
-	}
-	
-	public void addEntry(String entry) {
-		entries.add(entry);
 	}
 
 }
