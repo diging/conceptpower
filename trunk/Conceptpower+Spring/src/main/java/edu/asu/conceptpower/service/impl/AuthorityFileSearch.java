22e60a19ac73c24b709813d593afa45ddd70aace
jdamerow <jdamerow@03b148cf-524d-4926-b617-1f50de7337d5>
2014-04-10 23:45:36 +0000

    code clean up

---
+++ trunk/Conceptpower+Spring/src/main/java/edu/asu/conceptpower/service/impl/AuthorityFileSearch.java
@@ -0,0 +1,53 @@
+package edu.asu.conceptpower.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.asu.conceptpower.profile.ISearchResult;
+import edu.asu.conceptpower.profile.IService;
+import edu.asu.conceptpower.profile.IServiceRegistry;
+import edu.asu.conceptpower.service.IAuthorityFileSearch;
+import edu.asu.conceptpower.web.backing.SearchResultBackBean;
+
+/**
+ * This class query a specified service and returns the results.
+ * 
+ * @author rohit pendbhaje
+ * 
+ */
+
+@Service
+public class AuthorityFileSearch implements
+		IAuthorityFileSearch {
+
+	@Autowired
+	private IServiceRegistry serviceRegistry;
+
+	
+	/**
+	 * this method takes results from viafservice method and copy it in the
+	 * SearchResultBackBean
+	 * 
+	 * 
+	 * @param serviceId
+	 *            id of the service selected by the user
+	 * @param term
+	 *            term entered by the user
+	 * @return list of copied searchresults of SearchResultBackBean class
+	 *         returned from viaf service
+	 */
+	@Override
+	public List<ISearchResult> getSearchResultBackBeanList(
+			String serviceId, String term) {
+
+		IService serviceObj = serviceRegistry.getServiceObject(serviceId);
+		List<ISearchResult> searchResults = serviceObj.search(term);
+		
+		return searchResults;
+
+	}
+
+}
