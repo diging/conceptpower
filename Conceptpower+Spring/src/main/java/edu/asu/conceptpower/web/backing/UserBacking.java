ff134a6d957a729b2847f6bc861cfceadb2d5993
ambichetan <ambichetan@03b148cf-524d-4926-b617-1f50de7337d5>
2014-05-08 18:17:45 +0000

    modified use edit functionality

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/backing/UserBacking.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/backing/UserBacking.java
@@ -7,38 +7,45 @@ public class UserBacking {
 	private String password;
 	private String retypedPassword;
 	private boolean isAdmin;
-	
+
 	public UserBacking(String username, String name) {
 		super();
 		this.username = username;
 		this.name = name;
 	}
-	
+
 	public UserBacking() {
-		
+
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getRetypedPassword() {
 		return retypedPassword;
 	}
+
 	public void setRetypedPassword(String retypedPassword) {
 		this.retypedPassword = retypedPassword;
 	}
@@ -50,6 +57,5 @@ public class UserBacking {
 	public void setIsAdmin(boolean isAdmin) {
 		this.isAdmin = isAdmin;
 	}
-	
-	
+
 }
