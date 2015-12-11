77b630429eab9f0df90719e466d8dff843196d1f
mkarthik90 <mkarthik90@gmail.com>
2015-12-11 12:16:14 -0700

    Comitting interface for URIHelper

---
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/util/IURIHelper.java
@@ -0,0 +1,14 @@
+package edu.asu.conceptpower.util;
+
+import edu.asu.conceptpower.core.ConceptEntry;
+import edu.asu.conceptpower.core.ConceptType;
+
+public interface IURIHelper {
+
+    public String getURI(ConceptEntry entry);
+    public String getTypeURI(ConceptType type);
+    public String getTypeId(String typeUriOrId);
+    
+    
+    
+}
