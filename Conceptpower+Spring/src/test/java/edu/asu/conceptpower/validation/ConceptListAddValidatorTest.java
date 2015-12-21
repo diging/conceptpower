9d974f09432b3640777012167310ec73f239c037
mkarthik90 <mkarthik90@gmail.com>
2015-12-21 17:22:26 -0500

    Added test cases for validator

--- Conceptpower+Spring/src/test/java/edu/asu/conceptpower/validation/ConceptListAddValidatorTest.java
+++ Conceptpower+Spring/src/test/java/edu/asu/conceptpower/validation/ConceptListAddValidatorTest.java
@@ -12,53 +12,79 @@ import org.springframework.validation.ValidationUtils;
 
 import edu.asu.conceptpower.core.IConceptListManager;
 import edu.asu.conceptpower.web.ConceptListAddForm;
+import edu.asu.conceptpower.wordnet.Constants;
 import junit.framework.Assert;
 
 public class ConceptListAddValidatorTest {
 
     @Mock
     private IConceptListManager conceptListManager;
-    
+
     @InjectMocks
     private ConceptListAddValidator conceptListAddValidator;
-    
+
     private ConceptListAddForm emptyListName;
     private ConceptListAddForm emptyDescription;
     private ConceptListAddForm wordNetConcepts;
     private ConceptListAddForm existingListName;
-    
+
     @Before
-    public void init(){
+    public void init() {
         conceptListManager = Mockito.mock(IConceptListManager.class);
         MockitoAnnotations.initMocks(this);
-        
+
         emptyListName = new ConceptListAddForm();
         emptyListName.setListName("");
-        
+
+        emptyDescription = new ConceptListAddForm();
+        emptyDescription.setListName("test");
+        emptyDescription.setDescription("");
+
+        wordNetConcepts = new ConceptListAddForm();
+        wordNetConcepts.setListName(Constants.WORDNET_DICTIONARY);
+        wordNetConcepts.setDescription("WordNet Concept");
+
+        existingListName = new ConceptListAddForm();
+        existingListName.setListName("ExistingList");
+        existingListName.setDescription("Description");
+        existingListName.setOldListName("OldExistingList");
+        Mockito.when(conceptListManager.checkExistingConceptList("ExistingList")).thenReturn(true);
     }
-    
+
     @Test
     public void testValidListName() {
-     
         Errors errors = new BindException(emptyListName, "conceptListAddForm");
         ValidationUtils.invokeValidator(conceptListAddValidator, emptyListName, errors);
         Assert.assertEquals(2, errors.getFieldErrorCount());
         Assert.assertEquals(errors.getFieldError("listName").getCode(), "concept_name.required");
         Assert.assertEquals(errors.getFieldError("description").getCode(), "concept_description.required");
     }
-    
-    /*@Test
+
+    @Test
     public void testValidDescription() {
-        
+        Errors errors = new BindException(emptyDescription, "conceptListAddForm");
+        ValidationUtils.invokeValidator(conceptListAddValidator, emptyDescription, errors);
+        Assert.assertEquals(1, errors.getFieldErrorCount());
+        Assert.assertNull(errors.getFieldError("listName"));
+        Assert.assertEquals(errors.getFieldError("description").getCode(), "concept_description.required");
+
     }
-    
+
     @Test
     public void testWordNetConcepts() {
-        
+        Errors errors = new BindException(wordNetConcepts, "conceptListAddForm");
+        ValidationUtils.invokeValidator(conceptListAddValidator, wordNetConcepts, errors);
+        Assert.assertEquals(1, errors.getFieldErrorCount());
+        Assert.assertNull(errors.getFieldError("description"));
+        Assert.assertEquals(errors.getFieldError("listName").getCode(), "concept_name.wordnet");
     }
-    
+
     @Test
     public void testExistingListName() {
-        
-    }*/
+        Errors errors = new BindException(existingListName, "conceptListAddForm");
+        ValidationUtils.invokeValidator(conceptListAddValidator, existingListName, errors);
+        Assert.assertEquals(1, errors.getFieldErrorCount());
+        Assert.assertNull(errors.getFieldError("description"));
+        Assert.assertEquals(errors.getFieldError("listName").getCode(), "concept_unique.required");
+    }
 }
