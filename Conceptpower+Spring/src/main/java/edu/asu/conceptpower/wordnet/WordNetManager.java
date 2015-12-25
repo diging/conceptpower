e58b26b9cf63b02b9ea20af156a3c4c54e14ca08
mkarthik90 <mkarthik90@gmail.com>
2015-12-25 15:12:18 -0500

    Loaded the details into lucene documents

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/wordnet/WordNetManager.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/wordnet/WordNetManager.java
@@ -5,12 +5,29 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,28 +46,64 @@ public class WordNetManager {
 
 	@Autowired
 	private WordNetConfiguration configuration;
-
+	
+	@Autowired
+	private StandardAnalyzer analyzer;
+	
+	private Directory index = new RAMDirectory();
+	
 	private IDictionary dict;
 
 	private Map<String, POS> posMap;
 
 	@PostConstruct
-	public void init() throws IOException {
-		String wnhome = configuration.getWordnetPath();
-		String path = wnhome + File.separator + configuration.getDictFolder();
-		URL url = null;
+    public void init() throws IOException, org.apache.lucene.queryparser.classic.ParseException {
+        String wnhome = configuration.getWordnetPath();
+        String path = wnhome + File.separator + configuration.getDictFolder();
+        URL url = null;
 
-		url = new URL("file", null, path);
+        url = new URL("file", null, path);
 
-		dict = new Dictionary(url);
-		dict.open();
+        dict = new Dictionary(url);
+        dict.open();
 
-		posMap = new HashMap<String, POS>();
-		posMap.put(edu.asu.conceptpower.core.POS.NOUN, POS.NOUN);
-		posMap.put(edu.asu.conceptpower.core.POS.VERB, POS.VERB);
-		posMap.put(edu.asu.conceptpower.core.POS.ADVERB, POS.ADVERB);
-		posMap.put(edu.asu.conceptpower.core.POS.ADJECTIVE, POS.ADJECTIVE);
-	}
+        Iterator<IIndexWord> iterator = dict.getIndexWordIterator(POS.NOUN);
+        int j = 0;
+        
+
+        for (; iterator.hasNext();) {
+            j++;
+            IIndexWord indexWord = iterator.next();
+            List<IWordID> wordIdds = indexWord.getWordIDs();
+
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            IndexWriter w = new IndexWriter(index, config);
+            for (IWordID wordId : wordIdds) {
+                Document doc = new Document();
+                doc.add(new TextField("word", wordId.getLemma(), Field.Store.YES));
+                doc.add(new StringField("pos", wordId.getPOS().toString(), Field.Store.YES));
+                doc.add(new StringField("id", wordId.getSynsetID().toString(), Field.Store.YES));
+                IWord word = dict.getWord(wordId);
+                doc.add(new StringField("decription", word.getSynset().getGloss(), Field.Store.YES));
+                doc.add(new StringField("wordNetId", word.getID().toString(), Field.Store.YES));
+                w.addDocument(doc);
+                
+                System.out.println(wordId.getPOS() + " " + wordId.getLemma());
+            }
+
+            if (j == 10000)
+                break;
+
+            w.close();
+        }
+        // System.out.println(j);
+        posMap = new HashMap<String, POS>();
+        posMap.put(edu.asu.conceptpower.core.POS.NOUN, POS.NOUN);
+        posMap.put(edu.asu.conceptpower.core.POS.VERB, POS.VERB);
+        posMap.put(edu.asu.conceptpower.core.POS.ADVERB, POS.ADVERB);
+        posMap.put(edu.asu.conceptpower.core.POS.ADJECTIVE, POS.ADJECTIVE);
+
+    }
 
 	public ConceptEntry getConcept(String id) {
 
@@ -118,13 +171,45 @@ public class WordNetManager {
 		return entry;
 	}
 
-	protected ConceptEntry getConceptFromWordId(IWordID wordId) {
-		IWord word = dict.getWord(wordId);
-		ConceptEntry entry = null;
-		if (word != null)
-			entry = createConceptEntry(word);
-		return entry;
-	}
+    protected ConceptEntry getConceptFromWordId(IWordID wordId) {
+        ConceptEntry entry = new ConceptEntry();
+        try {
+            String word = wordId.getLemma();
+            String pos = wordId.getPOS().toString();
+
+            Query q = new QueryParser("title", analyzer).parse("word:" + word + " AND pos:" + pos);
+
+            int hitsPerPage = 10;
+            IndexReader reader = DirectoryReader.open(index);
+            IndexSearcher searcher = new IndexSearcher(reader);
+            TopDocs docs = searcher.search(q, hitsPerPage);
+            ScoreDoc[] hits = docs.scoreDocs;
+
+            System.out.println("Found " + hits.length + " hits.");
+            for (int i = 0; i < hits.length; ++i) {
+                int docId = hits[i].doc;
+                Document d = searcher.doc(docId);
+
+                entry.setId(d.get("id"));
+                entry.setWord(d.get("word"));
+                entry.setPos(d.get("pos"));
+                entry.setConceptList(Constants.WORDNET_DICTIONARY);
+                entry.setDescription(d.get("Description"));
+
+            }
+
+            IWord iWord = null;
+            /*
+             * ConceptEntry entry = null; if (word != null) entry =
+             * createConceptEntry();
+             */
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return entry;
+    }
 
 	public ConceptEntry[] getEntriesForWord(String word) {
 
