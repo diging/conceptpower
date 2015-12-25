d079c5b9bbad8219d6fb027d169103eb3603d621
mkarthik90 <mkarthik90@gmail.com>
2015-12-25 18:01:20 -0500

    Changes for loading the data to lucene

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/wordnet/WordNetManager.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/wordnet/WordNetManager.java
@@ -44,19 +44,19 @@ import edu.mit.jwi.item.WordID;
 @Service
 public class WordNetManager {
 
-	@Autowired
-	private WordNetConfiguration configuration;
-	
-	@Autowired
-	private StandardAnalyzer analyzer;
-	
-	private Directory index = new RAMDirectory();
-	
-	private IDictionary dict;
+    @Autowired
+    private WordNetConfiguration configuration;
 
-	private Map<String, POS> posMap;
+    @Autowired
+    private StandardAnalyzer analyzer;
 
-	@PostConstruct
+    private Directory index = new RAMDirectory();
+
+    private IDictionary dict;
+
+    private Map<String, POS> posMap;
+
+    @PostConstruct
     public void init() throws IOException, org.apache.lucene.queryparser.classic.ParseException {
         String wnhome = configuration.getWordnetPath();
         String path = wnhome + File.separator + configuration.getDictFolder();
@@ -68,11 +68,8 @@ public class WordNetManager {
         dict.open();
 
         Iterator<IIndexWord> iterator = dict.getIndexWordIterator(POS.NOUN);
-        int j = 0;
-        
 
         for (; iterator.hasNext();) {
-            j++;
             IIndexWord indexWord = iterator.next();
             List<IWordID> wordIdds = indexWord.getWordIDs();
 
@@ -82,21 +79,129 @@ public class WordNetManager {
                 Document doc = new Document();
                 doc.add(new TextField("word", wordId.getLemma(), Field.Store.YES));
                 doc.add(new StringField("pos", wordId.getPOS().toString(), Field.Store.YES));
-                doc.add(new StringField("id", wordId.getSynsetID().toString(), Field.Store.YES));
+
                 IWord word = dict.getWord(wordId);
-                doc.add(new StringField("decription", word.getSynset().getGloss(), Field.Store.YES));
-                doc.add(new StringField("wordNetId", word.getID().toString(), Field.Store.YES));
+                doc.add(new StringField("description", word.getSynset().getGloss(), Field.Store.YES));
+                doc.add(new StringField("id", word.getID().toString(), Field.Store.YES));
                 w.addDocument(doc);
-                
+
+                ISynset synset = word.getSynset();
+                List<IWord> synonyms = synset.getWords();
+                StringBuffer sb = new StringBuffer();
+                for (IWord syn : synonyms) {
+                    if (!syn.getID().equals(word.getID()))
+                        sb.append(syn.getID().toString() + edu.asu.conceptpower.core.Constants.SYNONYM_SEPARATOR);
+                }
+                doc.add(new StringField("synonymId", sb.toString(), Field.Store.YES));
+
                 System.out.println(wordId.getPOS() + " " + wordId.getLemma());
             }
 
-            if (j == 10000)
-                break;
-
             w.close();
         }
-        // System.out.println(j);
+
+        iterator = dict.getIndexWordIterator(POS.ADVERB);
+        for (; iterator.hasNext();) {
+
+            IIndexWord indexWord = iterator.next();
+            List<IWordID> wordIdds = indexWord.getWordIDs();
+
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            IndexWriter w = new IndexWriter(index, config);
+            for (IWordID wordId : wordIdds) {
+                Document doc = new Document();
+                doc.add(new TextField("word", wordId.getLemma(), Field.Store.YES));
+                doc.add(new StringField("pos", wordId.getPOS().toString(), Field.Store.YES));
+
+                IWord word = dict.getWord(wordId);
+                doc.add(new StringField("description", word.getSynset().getGloss(), Field.Store.YES));
+                doc.add(new StringField("id", word.getID().toString(), Field.Store.YES));
+                w.addDocument(doc);
+
+                ISynset synset = word.getSynset();
+                List<IWord> synonyms = synset.getWords();
+                StringBuffer sb = new StringBuffer();
+                for (IWord syn : synonyms) {
+                    if (!syn.getID().equals(word.getID()))
+                        sb.append(syn.getID().toString() + edu.asu.conceptpower.core.Constants.SYNONYM_SEPARATOR);
+                }
+                doc.add(new StringField("synonymId", sb.toString(), Field.Store.YES));
+
+                System.out.println(wordId.getPOS() + " " + wordId.getLemma());
+            }
+
+            w.close();
+
+        }
+
+        iterator = dict.getIndexWordIterator(POS.ADJECTIVE);
+        for (; iterator.hasNext();) {
+
+            IIndexWord indexWord = iterator.next();
+            List<IWordID> wordIdds = indexWord.getWordIDs();
+
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            IndexWriter w = new IndexWriter(index, config);
+            for (IWordID wordId : wordIdds) {
+                Document doc = new Document();
+                doc.add(new TextField("word", wordId.getLemma(), Field.Store.YES));
+                doc.add(new StringField("pos", wordId.getPOS().toString(), Field.Store.YES));
+
+                IWord word = dict.getWord(wordId);
+                doc.add(new StringField("description", word.getSynset().getGloss(), Field.Store.YES));
+                doc.add(new StringField("id", word.getID().toString(), Field.Store.YES));
+                w.addDocument(doc);
+
+                ISynset synset = word.getSynset();
+                List<IWord> synonyms = synset.getWords();
+                StringBuffer sb = new StringBuffer();
+                for (IWord syn : synonyms) {
+                    if (!syn.getID().equals(word.getID()))
+                        sb.append(syn.getID().toString() + edu.asu.conceptpower.core.Constants.SYNONYM_SEPARATOR);
+                }
+                doc.add(new StringField("synonymId", sb.toString(), Field.Store.YES));
+
+                System.out.println(wordId.getPOS() + " " + wordId.getLemma());
+            }
+
+            w.close();
+
+        }
+
+        iterator = dict.getIndexWordIterator(POS.VERB);
+        for (; iterator.hasNext();) {
+
+            IIndexWord indexWord = iterator.next();
+            List<IWordID> wordIdds = indexWord.getWordIDs();
+
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            IndexWriter w = new IndexWriter(index, config);
+            for (IWordID wordId : wordIdds) {
+                Document doc = new Document();
+                doc.add(new TextField("word", wordId.getLemma(), Field.Store.YES));
+                doc.add(new StringField("pos", wordId.getPOS().toString(), Field.Store.YES));
+
+                IWord word = dict.getWord(wordId);
+                doc.add(new StringField("description", word.getSynset().getGloss(), Field.Store.YES));
+                doc.add(new StringField("id", word.getID().toString(), Field.Store.YES));
+                w.addDocument(doc);
+
+                ISynset synset = word.getSynset();
+                List<IWord> synonyms = synset.getWords();
+                StringBuffer sb = new StringBuffer();
+                for (IWord syn : synonyms) {
+                    if (!syn.getID().equals(word.getID()))
+                        sb.append(syn.getID().toString() + edu.asu.conceptpower.core.Constants.SYNONYM_SEPARATOR);
+                }
+                doc.add(new StringField("synonymId", sb.toString(), Field.Store.YES));
+
+                System.out.println(wordId.getPOS() + " " + wordId.getLemma());
+            }
+
+            w.close();
+
+        }
+
         posMap = new HashMap<String, POS>();
         posMap.put(edu.asu.conceptpower.core.POS.NOUN, POS.NOUN);
         posMap.put(edu.asu.conceptpower.core.POS.VERB, POS.VERB);
@@ -105,187 +210,199 @@ public class WordNetManager {
 
     }
 
-	public ConceptEntry getConcept(String id) {
+    public ConceptEntry getConcept(String id) {
 
-		IWordID wordId = null;
-		try {
-			wordId = WordID.parseWordID(id);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+        IWordID wordId = null;
+        try {
+            wordId = WordID.parseWordID(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
 
-		if (wordId != null) {
-			ConceptEntry entry = getConceptFromWordId(wordId);
+        if (wordId != null) {
+            ConceptEntry entry = getConceptFromWordId(wordId);
 
-			return entry;
-		}
+            return entry;
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public ConceptEntry[] getSynonyms(String id) {
-		IWordID wordId = null;
-		try {
-			wordId = WordID.parseWordID(id);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+    public ConceptEntry[] getSynonyms(String id) {
+        IWordID wordId = null;
+        try {
+            wordId = WordID.parseWordID(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
 
-		List<ConceptEntry> entries = new ArrayList<ConceptEntry>();
-		if (wordId != null) {
-			IWord word = dict.getWord(wordId);
-			ISynset synset = word.getSynset();
-			if (synset != null) {
-				List<IWord> synonyms = synset.getWords();
-				for (IWord synonym : synonyms) {
-					entries.add(createConceptEntry(synonym));
-				}
-			}
-		}
+        List<ConceptEntry> entries = new ArrayList<ConceptEntry>();
+        if (wordId != null) {
+            IWord word = dict.getWord(wordId);
+            ISynset synset = word.getSynset();
+            if (synset != null) {
+                List<IWord> synonyms = synset.getWords();
+                for (IWord synonym : synonyms) {
+                    entries.add(createConceptEntry(synonym));
+                }
+            }
+        }
 
-		return entries.toArray(new ConceptEntry[entries.size()]);
-	}
+        return entries.toArray(new ConceptEntry[entries.size()]);
+    }
 
-	protected ConceptEntry createConceptEntry(IWord word) {
-		ConceptEntry entry = new ConceptEntry();
-		entry.setId(word.getID().toString());
-		entry.setWordnetId(word.getID().toString());
-		entry.setWord(word.getLemma());
-		entry.setPos(word.getPOS().name());
-		entry.setConceptList(Constants.WORDNET_DICTIONARY);
+    protected ConceptEntry createConceptEntry(IWord word) {
+        ConceptEntry entry = new ConceptEntry();
+        entry.setId(word.getID().toString());
+        entry.setWordnetId(word.getID().toString());
+        entry.setWord(word.getLemma());
+        entry.setPos(word.getPOS().name());
+        entry.setConceptList(Constants.WORDNET_DICTIONARY);
 
-		entry.setDescription(word.getSynset().getGloss());
+        entry.setDescription(word.getSynset().getGloss());
 
-		ISynset synset = word.getSynset();
-		List<IWord> synonyms = synset.getWords();
-		StringBuffer sb = new StringBuffer();
-		for (IWord syn : synonyms) {
-			if (!syn.getID().equals(word.getID()))
-				sb.append(syn.getID().toString()
-						+ edu.asu.conceptpower.core.Constants.SYNONYM_SEPARATOR);
-		}
-		entry.setSynonymIds(sb.toString());
+        ISynset synset = word.getSynset();
+        List<IWord> synonyms = synset.getWords();
+        StringBuffer sb = new StringBuffer();
+        for (IWord syn : synonyms) {
+            if (!syn.getID().equals(word.getID()))
+                sb.append(syn.getID().toString() + edu.asu.conceptpower.core.Constants.SYNONYM_SEPARATOR);
+        }
+        entry.setSynonymIds(sb.toString());
 
-		return entry;
-	}
+        return entry;
+    }
 
     protected ConceptEntry getConceptFromWordId(IWordID wordId) {
-        ConceptEntry entry = new ConceptEntry();
-        try {
-            String word = wordId.getLemma();
-            String pos = wordId.getPOS().toString();
+        IWord word = dict.getWord(wordId);
+        ConceptEntry entry = null;
+        if (word != null)
+            entry = createConceptEntry(word);
+        return entry;
 
-            Query q = new QueryParser("title", analyzer).parse("word:" + word + " AND pos:" + pos);
+    }
+
+    protected ConceptEntry getConceptFromDocument(Document d) {
+        ConceptEntry entry = new ConceptEntry();
+        entry.setId(d.get("id"));
+        entry.setWord(d.get("word"));
+        entry.setPos(d.get("pos"));
+        entry.setConceptList(Constants.WORDNET_DICTIONARY);
+        entry.setDescription(d.get("description"));
+        entry.setWordnetId(d.get("id"));
+        entry.setSynonymIds(d.get("synonymId"));
+        return entry;
+    }
+
+    public ConceptEntry[] getEntriesForWord(String word) {
+        try {
+            if (word == null || word.trim().isEmpty())
+                return new ConceptEntry[0];
+
+            POS pPOS = POS.NOUN;
+
+            List<ConceptEntry> concepts = new ArrayList<ConceptEntry>();
+            // get nouns
+
+            Query q = new QueryParser("title", analyzer).parse("word:" + word + " AND pos:" + pPOS.toString());
 
             int hitsPerPage = 10;
+            IndexReader reader = DirectoryReader.open(index);
+            IndexSearcher searcher = new IndexSearcher(reader);
+
+            TopDocs docs = searcher.search(q, hitsPerPage);
+            ScoreDoc[] hits = docs.scoreDocs;
+
+            for (int i = 0; i < hits.length; ++i) {
+                int docId = hits[i].doc;
+                Document d = searcher.doc(docId);
+                ConceptEntry entry = getConceptFromDocument(d);
+                concepts.add(entry);
+            }
+
+            IIndexWord indexWord = dict.getIndexWord(word, pPOS);
+
+            // get verbs
+            pPOS = POS.VERB;
+            q = new QueryParser("title", analyzer).parse("word:" + word + " AND pos:" + pPOS.toString());
+            docs = searcher.search(q, hitsPerPage);
+            hits = docs.scoreDocs;
+            for (int i = 0; i < hits.length; ++i) {
+                int docId = hits[i].doc;
+                Document d = searcher.doc(docId);
+                ConceptEntry entry = getConceptFromDocument(d);
+                concepts.add(entry);
+            }
+
+            // get adverbs
+            pPOS = POS.ADVERB;
+            q = new QueryParser("title", analyzer).parse("word:" + word + " AND pos:" + pPOS.toString());
+            docs = searcher.search(q, hitsPerPage);
+            hits = docs.scoreDocs;
+            for (int i = 0; i < hits.length; ++i) {
+                int docId = hits[i].doc;
+                Document d = searcher.doc(docId);
+                ConceptEntry entry = getConceptFromDocument(d);
+                concepts.add(entry);
+            }
+
+            // get adjectives
+            pPOS = POS.ADJECTIVE;
+            q = new QueryParser("title", analyzer).parse("word:" + word + " AND pos:" + pPOS.toString());
+            docs = searcher.search(q, hitsPerPage);
+            hits = docs.scoreDocs;
+            for (int i = 0; i < hits.length; ++i) {
+                int docId = hits[i].doc;
+                Document d = searcher.doc(docId);
+                ConceptEntry entry = getConceptFromDocument(d);
+                concepts.add(entry);
+            }
+
+            return concepts.toArray(new ConceptEntry[concepts.size()]);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public ConceptEntry[] getEntriesForWord(String word, String pos) {
+        POS pPOS = posMap.get(pos);
+        if (pPOS == null)
+            return null;
+
+        Query q;
+        try {
+            q = new QueryParser("title", analyzer).parse("word:" + word + " AND pos:" + pPOS.toString());
+
+            int hitsPerPage = 10;
+            List<ConceptEntry> concepts = new ArrayList<ConceptEntry>();
+
             IndexReader reader = DirectoryReader.open(index);
             IndexSearcher searcher = new IndexSearcher(reader);
             TopDocs docs = searcher.search(q, hitsPerPage);
             ScoreDoc[] hits = docs.scoreDocs;
 
-            System.out.println("Found " + hits.length + " hits.");
             for (int i = 0; i < hits.length; ++i) {
                 int docId = hits[i].doc;
                 Document d = searcher.doc(docId);
-
-                entry.setId(d.get("id"));
-                entry.setWord(d.get("word"));
-                entry.setPos(d.get("pos"));
-                entry.setConceptList(Constants.WORDNET_DICTIONARY);
-                entry.setDescription(d.get("Description"));
-
+                ConceptEntry entry = getConceptFromDocument(d);
+                concepts.add(entry);
             }
 
-            IWord iWord = null;
-            /*
-             * ConceptEntry entry = null; if (word != null) entry =
-             * createConceptEntry();
-             */
+            return concepts.toArray(new ConceptEntry[concepts.size()]);
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
-        return entry;
+        return null;
     }
 
-	public ConceptEntry[] getEntriesForWord(String word) {
-
-		if (word == null || word.trim().isEmpty())
-			return new ConceptEntry[0];
-
-		POS pPOS = POS.NOUN;
-
-		List<ConceptEntry> concepts = new ArrayList<ConceptEntry>();
-		// get nouns
-		IIndexWord indexWord = dict.getIndexWord(word, pPOS);
-		if (indexWord != null) {
-			List<IWordID> wordIDs = indexWord.getWordIDs();
-			for (IWordID wid : wordIDs) {
-				ConceptEntry entry = getConceptFromWordId(wid);
-				concepts.add(entry);
-			}
-		}
-
-		// get verbs
-		pPOS = POS.VERB;
-		indexWord = dict.getIndexWord(word, pPOS);
-		if (indexWord != null) {
-			List<IWordID> wordIDs = indexWord.getWordIDs();
-			for (IWordID wid : wordIDs) {
-				ConceptEntry entry = getConceptFromWordId(wid);
-				concepts.add(entry);
-			}
-		}
-
-		// get adverbs
-		pPOS = POS.ADVERB;
-		indexWord = dict.getIndexWord(word, pPOS);
-		if (indexWord != null) {
-			List<IWordID> wordIDs = indexWord.getWordIDs();
-			for (IWordID wid : wordIDs) {
-				ConceptEntry entry = getConceptFromWordId(wid);
-				concepts.add(entry);
-			}
-		}
-
-		// get adjectives
-		pPOS = POS.ADJECTIVE;
-		indexWord = dict.getIndexWord(word, pPOS);
-		if (indexWord != null) {
-			List<IWordID> wordIDs = indexWord.getWordIDs();
-			for (IWordID wid : wordIDs) {
-				ConceptEntry entry = getConceptFromWordId(wid);
-				concepts.add(entry);
-			}
-		}
-
-		return concepts.toArray(new ConceptEntry[concepts.size()]);
-	}
-
-	public ConceptEntry[] getEntriesForWord(String word, String pos) {
-		POS pPOS = posMap.get(pos);
-		if (pPOS == null)
-			return null;
-
-		List<ConceptEntry> concepts = new ArrayList<ConceptEntry>();
-		IIndexWord indexWord = dict.getIndexWord(word, pPOS);
-		if (indexWord != null) {
-			List<IWordID> wordIDs = indexWord.getWordIDs();
-			for (IWordID wid : wordIDs) {
-				ConceptEntry entry = getConceptFromWordId(wid);
-				concepts.add(entry);
-			}
-			return concepts.toArray(new ConceptEntry[concepts.size()]);
-		}
-
-		return null;
-	}
-
-	@PreDestroy
-	public void close() {
-		dict.close();
-	}
+    @PreDestroy
+    public void close() {
+        dict.close();
+    }
 }
