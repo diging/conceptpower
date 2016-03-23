51151aa44596926ee51bdf09a81216369e722da2
karthikeyanmohan <mkarthik90@gmail.com>
2016-03-23 09:49:52 -0700

    Changes for merge conflict

---
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/servlet/rdf/RDFConceptMessage.java
@@ -0,0 +1,12 @@
+package edu.asu.conceptpower.servlet.rdf;
+
+import edu.asu.conceptpower.root.URIHelper;
+
+public class RDFConceptMessage {
+
+    private URIHelper creator;
+    
+    public RDFConceptMessage(URIHelper creator){
+        this.creator = creator;
+    }
+}
