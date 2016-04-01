207f98581483c6c7c0ed8b0b0639ba472a2df581
karthikeyanmohan <mkarthik90@gmail.com>
2016-04-01 16:29:38 -0700

    Resolved merge conflicts and publishing errors

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/root/UserService.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/root/UserService.java
@@ -13,35 +13,26 @@ import edu.asu.conceptpower.servlet.users.ConceptpowerGrantedAuthority;
 import edu.asu.conceptpower.servlet.users.IUserManager;
 import edu.asu.conceptpower.servlet.users.User;
 
-@Service
+@Service("userService")
 public class UserService implements UserDetailsService {
 
     @Autowired
-	private IUserManager userManager;
-
-    @Override
-	public UserDetails loadUserByUsername(String arg0)
-			throws UsernameNotFoundException {
-		User user = userManager.findUser(arg0);
-		
-		if (user == null)
-			throw new UsernameNotFoundException("Couldn't find username.");
-		
-		List<ConceptpowerGrantedAuthority> roles = new ArrayList<ConceptpowerGrantedAuthority>();
-		roles.add(new ConceptpowerGrantedAuthority(ConceptpowerGrantedAuthority.ROLE_USER));
-		if (user.getIsAdmin())
-			roles.add(new ConceptpowerGrantedAuthority(ConceptpowerGrantedAuthority.ROLE_ADMIN));
-		
-		UserDetails details = new CPUserDetails(user.getUsername(), user.getFullname(), user.getPw(), roles, user.getEmail());
-		return details;
-	}
+    private IUserManager userManager;
     
-
-    public IUserManager getUserManager() {
-        return userManager;
-    }
-
-    public void setUserManager(IUserManager userManager) {
-        this.userManager = userManager;
+    @Override
+    public UserDetails loadUserByUsername(String arg0)
+            throws UsernameNotFoundException {
+        User user = userManager.findUser(arg0);
+        
+        if (user == null)
+            throw new UsernameNotFoundException("Couldn't find username.");
+        
+        List<ConceptpowerGrantedAuthority> roles = new ArrayList<ConceptpowerGrantedAuthority>();
+        roles.add(new ConceptpowerGrantedAuthority(ConceptpowerGrantedAuthority.ROLE_USER));
+        if (user.getIsAdmin())
+            roles.add(new ConceptpowerGrantedAuthority(ConceptpowerGrantedAuthority.ROLE_ADMIN));
+        
+        UserDetails details = new CPUserDetails(user.getUsername(), user.getFullname(), user.getPw(), roles, user.getEmail());
+        return details;
     }
 }
