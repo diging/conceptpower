18b5417c15c093c7b2f6a2feb35808bf108777bc
Karthikeyan Mohan <mkarthik90@gmail.com>
2016-11-03 14:37:41 -0700

    [CCP-181] Changes for concept id lookup

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/servlet/rest/ConceptIDLookup.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/servlet/rest/ConceptIDLookup.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +21,7 @@ import edu.asu.conceptpower.core.ConceptEntry;
 import edu.asu.conceptpower.core.ConceptType;
 import edu.asu.conceptpower.root.TypeDatabaseClient;
 import edu.asu.conceptpower.servlet.core.IConceptManager;
+import edu.asu.conceptpower.servlet.json.JsonConceptMessage;
 import edu.asu.conceptpower.servlet.xml.MessageFactory;
 import edu.asu.conceptpower.servlet.xml.XMLConceptMessage;
 
@@ -52,8 +54,9 @@ public class ConceptIDLookup {
 	 *            Holds the HTTP request information
 	 * @return XML containing concept information
 	 */
-	@RequestMapping(value = "rest/Concept", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE + "; charset=utf-8")
-    public @ResponseBody ResponseEntity<String> getConceptById(HttpServletRequest req) {
+    @RequestMapping(value = "rest/Concept", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE + ","
+            + MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<String> getConceptById(HttpServletRequest req, @RequestHeader(value="Accept", defaultValue=MediaType.APPLICATION_XML_VALUE) String acceptHeader) {
 
         String id = req.getParameter("id");
         if (id == null || id.trim().isEmpty()) {
@@ -73,19 +76,36 @@ public class ConceptIDLookup {
         Map<ConceptEntry, ConceptType> entryMap = new HashMap<ConceptEntry, ConceptType>();
         List<String> xmlEntries = new ArrayList<String>();
 
-        XMLConceptMessage msg = messageFactory.getXMLMessageFactory().createXMLConceptMessage();
+        if (acceptHeader.equalsIgnoreCase(MediaType.APPLICATION_XML_VALUE)) {
+            // XML
+            XMLConceptMessage msg = messageFactory.getXMLMessageFactory().createXMLConceptMessage();
+            if (entry != null) {
 
-        if (entry != null) {
-
-            ConceptType type = null;
-
-            if (typeManager != null && entry.getTypeId() != null && !entry.getTypeId().trim().isEmpty()) {
-                type = typeManager.getType(entry.getTypeId());
+                ConceptType type = null;
+                if (typeManager != null && entry.getTypeId() != null && !entry.getTypeId().trim().isEmpty()) {
+                    type = typeManager.getType(entry.getTypeId());
+                }
+                entryMap.put(entry, type);
+                xmlEntries = msg.appendEntries(entryMap);
             }
-            entryMap.put(entry, type);
-            xmlEntries = msg.appendEntries(entryMap);
+
+            return new ResponseEntity<String>(msg.getXML(xmlEntries), HttpStatus.OK);
+        } else {
+            // JSON
+            JsonConceptMessage msg = messageFactory.getJsonMessageFactory().createJsonConceptMessage();
+
+            if (entry != null) {
+
+                ConceptType type = null;
+                if (typeManager != null && entry.getTypeId() != null && !entry.getTypeId().trim().isEmpty()) {
+                    type = typeManager.getType(entry.getTypeId());
+                }
+                entryMap.put(entry, type);
+                return new ResponseEntity<String>(msg.getJsonArray(entryMap), HttpStatus.OK);
+            }
+
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
         }
-        
-        return new ResponseEntity<String>(msg.getXML(xmlEntries), HttpStatus.OK);
+
     }
 }
