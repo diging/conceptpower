c82fce2f619eaf82d0c2a1ba076bf3c325f7fcd0
Karthikeyan Mohan <mkarthik90@gmail.com>
2016-11-09 12:06:33 -0700

    [CCP-176] Created exception handlers.
    1) Exception handlers are created for rest controllers as well as
    UI controllers.

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/app/util/ExceptionHandler.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/app/util/ExceptionHandler.java
@@ -1,10 +1,9 @@
 package edu.asu.conceptpower.app.util;
 
+import java.nio.file.AccessDeniedException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
@@ -18,35 +17,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ExceptionHandler {
 	
-	private static final Logger logger = LoggerFactory
-			.getLogger(ExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
-	@org.springframework.web.bind.annotation.ExceptionHandler(value = BadCredentialsException.class)
-	public ResponseEntity<String> handleAuthenticationFailure(BadCredentialsException ex) {
-		return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
-	}
-	
-	@org.springframework.web.bind.annotation.ExceptionHandler(value = AccessDeniedException.class)
-	public ResponseEntity<String> handleAccessDenied(AccessDeniedException ex) {
-		return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
-	}
-	
-	@org.springframework.web.bind.annotation.ExceptionHandler(value = Exception.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = { Exception.class, IllegalArgumentException.class,
+            IllegalAccessException.class, BadCredentialsException.class, AccessDeniedException.class })
 	public String handle(Exception e) {
 		logger.error("ExceptionHandler caught exception.", e);
 		return "exception";
 	}
-	
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        logger.error("Illegal argument exception.", ex);
-        return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = IllegalAccessException.class)
-    public ResponseEntity<String> handleIllegalAccessException(IllegalAccessException ex) {
-        logger.error("Illegal access exception.", ex);
-        return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
 }
