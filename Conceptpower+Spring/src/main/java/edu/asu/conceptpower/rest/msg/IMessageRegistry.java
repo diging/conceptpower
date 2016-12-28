a5a2bd001bed2c05bc45dea887089187e5088c05
Julia Damerow <jdamerow@users.noreply.github.com>
2016-12-28 09:21:58 -0700

    [CCP-181] javadoc

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/rest/msg/IMessageRegistry.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/rest/msg/IMessageRegistry.java
@@ -10,10 +10,11 @@ public interface IMessageRegistry {
 
     /**
      * This method returns the message converter object based on the media type.
-     * Valid values for media types are accept headers of XML and JSON.
+     * Valid values for media types are defined by the registered message converters.
+     * Currently, registered media types are for XML or JSON.
      * 
-     * @param mediaType
-     * @return
+     * @param mediaType application/xml or application/json
+     * @return Message converter according to provided media type or null.
      */
     public IMessageConverter getMessageFactory(String mediaType);
 
