5c90decaf3e288527f1e198fc8c922bf61c26213
Karthikeyan Mohan <mkarthik90@gmail.com>
2017-03-07 09:22:29 -0700

    [CCP-202] Added error test cases for synonym and type id

--- Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/rest/SynonymSearchIT.java
+++ Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/rest/SynonymSearchIT.java
@@ -15,7 +15,7 @@ import edu.asu.conceptpower.IntegrationTest;
 public class SynonymSearchIT extends IntegrationTest {
 
     @Test
-    public void getSynonymByIdTest() throws Exception {
+    public void test_getSynonymsForId_success() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/SynonymSearch").accept(MediaType.APPLICATION_JSON_VALUE)
                 .with(new RequestPostProcessor() {
                     public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
@@ -29,6 +29,27 @@ public class SynonymSearchIT extends IntegrationTest {
                 .andExpect(jsonPath("$.conceptEntries[1].pos", is("NOUN")))
                 .andExpect(jsonPath("$.conceptEntries[1].synonym_ids", is("WID-02382039-N-02-Indian_pony,")))
                 .andExpect(jsonPath("$.conceptEntries[0].id", is("CONfm3IOy7p4tDk")));
+    }
+
+    @Test
+    public void test_getSynonymsForId_emptySynonymId() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/SynonymSearch").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", "");
+                        return request;
+                    }
+                })).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void test_getSynonymsForId_nullSynonymId() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/SynonymSearch").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        return request;
+                    }
+                })).andExpect(status().isBadRequest());
     }
 
 }
--- Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/rest/TypeIdLookUpIT.java
+++ Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/rest/TypeIdLookUpIT.java
@@ -11,11 +11,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 import edu.asu.conceptpower.IntegrationTest;
+import edu.asu.conceptpower.rest.msg.xml.XMLConstants;
 
 public class TypeIdLookUpIT extends IntegrationTest {
 
     @Test
-    public void getTypeByIdTest() throws Exception {
+    public void test_getTypeById_successWithId() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
                 .with(new RequestPostProcessor() {
                     public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
@@ -27,6 +28,54 @@ public class TypeIdLookUpIT extends IntegrationTest {
                 .andExpect(jsonPath("$.type_uri",
                         is("http://www.digitalhps.org/types/TYPE_7c8745be-d06f-4feb-b749-910efa1b986d")))
                 .andExpect(jsonPath("$.creator_id", is("admin"))).andExpect(jsonPath("$.description", is("Test-Type")));
+    }
+
+    @Test
+    public void test_getTypeById_successWithTypeId() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", XMLConstants.TYPE_PREFIX + "7c8745be-d06f-4feb-b749-910efa1b986d");
+                        return request;
+                    }
+                })).andExpect(status().isOk())
+                .andExpect(jsonPath("$.type_id", is("7c8745be-d06f-4feb-b749-910efa1b986d")))
+                .andExpect(jsonPath("$.type_uri",
+                        is("http://www.digitalhps.org/types/TYPE_7c8745be-d06f-4feb-b749-910efa1b986d")))
+                .andExpect(jsonPath("$.creator_id", is("admin"))).andExpect(jsonPath("$.description", is("Test-Type")));
+    }
+
+    @Test
+    public void test_getTypeById_emptyResult() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", XMLConstants.TYPE_PREFIX + "7c8745be-d06f-4feb-b749-910efa1b986e");
+                        return request;
+                    }
+                })).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void test_getTypeById_nullTypeId() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        return request;
+                    }
+                })).andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    public void test_getTypeById_emptyTypeId() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", "");
+                        return request;
+                    }
+                })).andExpect(status().isNotFound());
     }
 
 }
