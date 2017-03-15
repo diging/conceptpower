148a31ba0eb4939eff93cad8519d9cce81fae462
Karthikeyan Mohan <mkarthik90@gmail.com>
2017-03-15 15:59:24 -0700

    [CCP-202-master] Added security context for luceneDAO

--- Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/IntegrationTest.java
+++ Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/IntegrationTest.java
@@ -1,10 +1,17 @@
 package edu.asu.conceptpower;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.TestPropertySource;
@@ -15,6 +22,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import edu.asu.conceptpower.servlet.users.ConceptpowerGrantedAuthority;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestPropertySource(locations = "classpath:/test.properties")
@@ -41,12 +50,24 @@ public abstract class IntegrationTest {
     public void setup() throws Exception {
 
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-
+        setSecurityContext();
         if (!isSetupDone) {
             this.mockMvc.perform(MockMvcRequestBuilders.post("/auth/indexConcepts").principal(principal));
             MvcResult mr = this.mockMvc.perform(MockMvcRequestBuilders.post("/auth/getIndexerStatus")).andReturn();
             isSetupDone = true;
         }
+    }
+
+    private void setSecurityContext() {
+        Collection<ConceptpowerGrantedAuthority> authorities = new ArrayList<>();
+        ConceptpowerGrantedAuthority grantedAuthority = new ConceptpowerGrantedAuthority("admin");
+        grantedAuthority.setAuthority(ConceptpowerGrantedAuthority.ROLE_ADMIN);
+        authorities.add(grantedAuthority);
+
+        AbstractAuthenticationToken auth = new AnonymousAuthenticationToken("admin", principal, authorities);
+        SecurityContext context = new SecurityContextImpl();
+        context.setAuthentication(auth);
+        SecurityContextHolder.setContext(context);
     }
 
 }
--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/servlet/lucene/impl/LuceneDAO.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/servlet/lucene/impl/LuceneDAO.java
@@ -6,6 +6,8 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
@@ -40,9 +42,8 @@ public class LuceneDAO implements ILuceneDAO {
      * Stores the number of indexed word count along with the timestamp
      */
     public void storeValues(long numberOfIndexedWords, String action) {
-        // Authentication auth =
-        // SecurityContextHolder.getContext().getAuthentication();
-        IndexingEvent bean = new IndexingEvent(new Date(), numberOfIndexedWords, action, "test");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        IndexingEvent bean = new IndexingEvent(new Date(), numberOfIndexedWords, action, auth.getName());
         luceneClient.store(bean);
         luceneClient.commit();
     }
