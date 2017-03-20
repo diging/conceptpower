bc23263075b5469dd2097e03de865ec36a930420
Karthikeyan Mohan <mkarthik90@gmail.com>
2017-03-20 08:49:13 -0700

    [CCP-202] Reverted to master branch code base.

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/servlet/rest/SearchParamters.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/servlet/rest/SearchParamters.java
@@ -3,6 +3,6 @@ package edu.asu.conceptpower.servlet.rest;
 public interface SearchParamters {
 
 	public final static String OPERATOR = "operator";
-    public final static String OP_AND = "AND";
-    public final static String OP_OR = "OR";
+    public final static String OP_AND = "and";
+    public final static String OP_OR = "or";
 }
