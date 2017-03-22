86cb52ecd71515e92391282ffd6a6b21415ec458
Karthikeyan Mohan <mkarthik90@gmail.com>
2017-03-22 13:54:27 -0700

    [CCP-202] Changes for indexing in integration test

--- Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/IntegrationTest.java
+++ Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/IntegrationTest.java
@@ -7,12 +7,14 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -42,13 +44,14 @@ public abstract class IntegrationTest {
 
     @Before
     public void setup() throws Exception {
-
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
         if (!isSetupDone) {
-            logger.debug("Started with lucene indexing for integration tests.");
             this.mockMvc.perform(MockMvcRequestBuilders.post("/auth/indexConcepts").principal(principal));
+            MvcResult mr = null;
+            do {
+                mr = this.mockMvc.perform(MockMvcRequestBuilders.post("/auth/getIndexerStatus")).andReturn();
+            } while (mr.getResponse().getStatus() != HttpStatus.OK.value());
             isSetupDone = true;
-            logger.debug("Completed with lucene indexing for integration tests.");
         }
     }
 }
