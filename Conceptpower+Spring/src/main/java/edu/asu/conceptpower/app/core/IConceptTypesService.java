623563237099d7a45a1c24bf965cc001480c0430
Karthikeyan <kmohan6@asu.edu>
2017-03-22 16:57:08 -0700

    [CCP-185] Changes for displaying alternative ids in all rest calls (#157)
    * [CCP-185] Changes for displaying alternative ids in all rest calls
    
    * [CCP-185] Review comments fixes
    
    Created interface for missing classes. Moved ConceptTypeServices to
    correct package.
    
    Added test cases for alternativeIDService class.
    
    * [CCP-185] Build failure fixes
    
    * [CCP-185] Added java docs for the alternativeIdService.
    
    * [CCP-185] Made changes to javadoc and test case
    
    Java doc changes and test case for queriedId being null.
    
    * [CCP-185] Review comments changes
    
    Renamed the conceptType enum to TypeOfId.
    Changes to javadocs.

---
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/app/core/IConceptTypesService.java
@@ -0,0 +1,32 @@
+package edu.asu.conceptpower.app.core;
+
+
+public interface IConceptTypesService {
+    
+    /**
+     * This enum contains the list of valid concept types.
+     * 
+     * @author karthikeyanmohan
+     */
+    public enum IdType {
+        GENERIC_WORDNET_CONCEPT_ID, SPECIFIC_WORDNET_CONCEPT_ID, LOCAL_CONCEPT_ID
+    }
+
+    /**
+     * This method returns the concept type based on the id parameter.
+     * 
+     * The different IdTypes that are returned by this method are
+     * 
+     * 1) GENERIC_WORDNET_CONCEPT_ID -> This id usually contains '??'.
+     * 
+     * 2) SPECIFIC_WORDNET_CONCEPT_ID -> This id usually starts with WID and
+     * does not contain any ??.
+     * 
+     * 3) LOCAL_CONCEPT_ID -> This is usually the concept id local to concept
+     * power.
+     * 
+     * @param id
+     * @return IdType
+     */
+    public IdType getConceptTypeByConceptId(String id);
+}
--- Conceptpower+Spring/src/test/java/edu/asu/conceptpower/rest/ConceptIDLookupTest.java
+++ Conceptpower+Spring/src/test/java/edu/asu/conceptpower/rest/ConceptIDLookupTest.java
@@ -17,9 +17,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.xml.sax.SAXException;
 
-import edu.asu.conceptpower.app.core.ConceptTypesService;
-import edu.asu.conceptpower.app.core.ConceptTypesService.ConceptTypes;
 import edu.asu.conceptpower.app.core.IConceptManager;
+import edu.asu.conceptpower.app.core.IConceptTypesService;
+import edu.asu.conceptpower.app.core.IConceptTypesService.IdType;
 import edu.asu.conceptpower.app.db.TypeDatabaseClient;
 import edu.asu.conceptpower.app.util.URIHelper;
 import edu.asu.conceptpower.core.ConceptEntry;
@@ -31,9 +31,6 @@ import junit.framework.Assert;
 public class ConceptIDLookupTest {
 
     @Mock
-    private IConceptManager dictManager;
-
-    @Mock
     private TypeDatabaseClient typeManager;
 
     @Mock
@@ -43,7 +40,7 @@ public class ConceptIDLookupTest {
     private IConceptManager conceptManager;
 
     @Mock
-    private ConceptTypesService conceptTypesService;
+    private IConceptTypesService conceptTypesService;
 
     @Mock
     private URIHelper uriCreator;
@@ -72,7 +69,7 @@ public class ConceptIDLookupTest {
         ConceptEntry entry = new ConceptEntry();
         entry.setId(conceptId);
         entry.setWordnetId(wordNetIds);
-        Mockito.when(dictManager.getConceptEntry(conceptId)).thenReturn(entry);
+        Mockito.when(conceptManager.getConceptEntry(conceptId)).thenReturn(entry);
         ResponseEntity<String> response = conceptIDLookup
                 .getConceptById("http://www.digitalhps.org/concepts/" + conceptId, MediaType.APPLICATION_XML_VALUE);
         RestTestUtility.testValidXml(response.getBody());
@@ -86,26 +83,23 @@ public class ConceptIDLookupTest {
                 this.getClass().getClassLoader().getResourceAsStream("unitTestOutput/alternativeGenericIds.xml"));
         final String wordNetIds = "W-ID01, W-ID02";
         final String conceptId = "CONf375adff-dde7-4536-9e62-f80328f800d0";
-        ConceptEntry entry = new ConceptEntry();
-        entry.setId("W-ID01");
-        entry.setWordnetId(wordNetIds);
-        Mockito.when(dictManager.getConceptEntry("W-ID??")).thenReturn(entry);
+
         Mockito.when(conceptTypesService.getConceptTypeByConceptId("W-ID??"))
-                .thenReturn(ConceptTypes.GENERIC_WORDNET_CONCEPT);
+                .thenReturn(IdType.GENERIC_WORDNET_CONCEPT_ID);
 
-        ConceptEntry newEntry = new ConceptEntry();
+        ConceptEntry entry = new ConceptEntry();
 
-        newEntry.setId(conceptId);
-        newEntry.setTypeId("T-123");
-        newEntry.setDescription("Description");
-        newEntry.setSynonymIds("SYN-01");
-        newEntry.setEqualTo("equals");
-        newEntry.setSimilarTo("similar");
-        newEntry.setPos("NOUN");
-        newEntry.setWord("pony");
-        newEntry.setWordnetId(wordNetIds);
+        entry.setId(conceptId);
+        entry.setTypeId("T-123");
+        entry.setDescription("Description");
+        entry.setSynonymIds("SYN-01");
+        entry.setEqualTo("equals");
+        entry.setSimilarTo("similar");
+        entry.setPos("NOUN");
+        entry.setWord("pony");
+        entry.setWordnetId(wordNetIds);
 
-        Mockito.when(conceptManager.getConceptEntry("W-ID01")).thenReturn(newEntry);
+        Mockito.when(conceptManager.getConceptEntry("W-ID??")).thenReturn(entry);
         ResponseEntity<String> response = conceptIDLookup.getConceptById("W-ID??", MediaType.APPLICATION_XML_VALUE);
         RestTestUtility.testValidXml(response.getBody());
         Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
