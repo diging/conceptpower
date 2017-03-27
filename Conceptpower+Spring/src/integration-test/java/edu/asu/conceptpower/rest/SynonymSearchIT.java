08d8139050cd9c02217ded080c4232e7262c312f
Karthikeyan <kmohan6@asu.edu>
2017-03-27 09:33:30 -0700

    Story/CCP-202 (#161)
    * [CCP-202] Added first integration test for restful calls.
    
    Integration tests run on a separate database. Properties
    are also loaded separately for test files.
    
    * [CCP-202] Review comments changes
    
    Added new class for separating servlet context and rest context. But was not able
    to create two different classes, because restful calls also needed lucene index which
    is available in servlet context.
    
    Added database files to the workspace and read from workspace.
    
    Added test case for the first restful call and validated the output from server.
    
    * [CCP-202] Added test cases for concept type and synonym search
    
    * [CCP-202] Added integration test for concept search
    
    Made sure all the test cases are running as expected. Integration test
    currently tests only for success scenarios.
    
    * [CCP-202] Refactor
    
    Creating separate source folders for integration tests.
    
    * [CCP-202] Changes for running integration tests while building.
    
    * [CCP-202] Formatting changes
    
    * [CCP-202] Added error test cases.
    
    Added test cases for error conditions. Previously the integration test contained only
    success scenarios.
    
    * [CCP-202] Added error test cases for Concepts.java
    
    * [CCP-202] Added error test cases for synonym and type id
    
    * [CCP-202] Review comments fix
    
    Removed the hardcoded value in word net configuration
    Added external files for reading the data.
    
    * [CCP-202] Changed the version
    
    * [CCP-202] Changes for unit test file names
    
    * [CCP-202] File name changes for unit test case
    
    * [CCP-202] Changes for testing xml responses.
    
    * [CCP-202] Review changes
    
    Introduced a smaller dataset from production database for running
    integration tests.
    
    ConceptList will be tokenized since exact match search is not required/
    If user searches for test we will return the entries from test and "test 1". And
    when user searches for "test 1" we return the entries only from "test 1"
    
    * [story/CCP-202] Review comments changes
    
    DB files for integration tests are downloaded from drop box.
    Changed the hardcode of dbpath for lucene db in integration test.
    
    * [CCP-202] Review comments changes
    
    Downloads url is configurable through maven.
    Formatting changes
    
    * [CCP-202] Correct url values for loading database files
    
    * [CCP-202] Added the following changes
    
    Pagination changes
    Alternative id changes
    Included the changes for json return type.
    
    * [CCP-202] Removed unwanted changes
    
    Reverted the changes made to concept entry.
    Search by concept list has not been included for now.
    
    * [CCP-202] Changes for json file.
    
    * [CCP-202] Changed to correct file name.
    
     Case sensitivity problem.
    
    * [CCP-202] Changes for output files to match wordnet3.0
    
    * [CCP-202] Added test case for testing default operator

--- Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/rest/SynonymSearchIT.java
+++ Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/rest/SynonymSearchIT.java
@@ -15,6 +15,40 @@ import edu.asu.conceptpower.IntegrationTest;
 public class SynonymSearchIT extends IntegrationTest {
 
     @Test
+    public void test_getSynonymsForId_successInJson() throws Exception {
+        final String output = IOUtil
+                .toString(this.getClass().getClassLoader().getResourceAsStream("output/synonym.json"));
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/SynonymSearch").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", "WID-02382039-N-02-Indian_pony");
+                        return request;
+                    }
+                })).andExpect(content().string(output)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void test_getSynonymsForId_emptySynonymIdInJson() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/SynonymSearch").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", "");
+                        return request;
+                    }
+                })).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void test_getSynonymsForId_nullSynonymIdInJson() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/SynonymSearch").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        return request;
+                    }
+                })).andExpect(status().isBadRequest());
+    }
+
+    @Test
     public void test_getSynonymsForId_successInXml() throws Exception {
         final String output = IOUtil
                 .toString(this.getClass().getClassLoader().getResourceAsStream("output/synonym.xml"));
@@ -47,6 +81,5 @@ public class SynonymSearchIT extends IntegrationTest {
                     }
                 })).andExpect(status().isBadRequest());
     }
-
 
 }
--- Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/rest/TypeIdLookUpIT.java
+++ Conceptpower+Spring/src/integration-test/java/edu/asu/conceptpower/rest/TypeIdLookUpIT.java
@@ -16,6 +16,65 @@ import edu.asu.conceptpower.rest.msg.xml.XMLConstants;
 public class TypeIdLookUpIT extends IntegrationTest {
 
     @Test
+    public void test_getTypeById_successWithIdInJson() throws Exception {
+        final String output = IOUtil
+                .toString(this.getClass().getClassLoader().getResourceAsStream("output/types.json"));
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", "986a7cc9-c0c1-4720-b344-853f08c136ab");
+                        return request;
+                    }
+                })).andExpect(content().string(output)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void test_getTypeById_successWithTypeIdInJson() throws Exception {
+        final String output = IOUtil
+                .toString(this.getClass().getClassLoader().getResourceAsStream("output/types.json"));
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", XMLConstants.TYPE_PREFIX + "986a7cc9-c0c1-4720-b344-853f08c136ab");
+                        return request;
+                    }
+                })).andExpect(content().string(output)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void test_getTypeById_emptyResultInJson() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", XMLConstants.TYPE_PREFIX + "7c8745be-d06f-4feb-b749-910efa1b986e");
+                        return request;
+                    }
+                })).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void test_getTypeById_nullTypeIdInJson() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        return request;
+                    }
+                })).andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    public void test_getTypeById_emptyTypeIdInJson() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_JSON_VALUE)
+                .with(new RequestPostProcessor() {
+                    public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                        request.setParameter("id", "");
+                        return request;
+                    }
+                })).andExpect(status().isNotFound());
+    }
+
+    @Test
     public void test_getTypeById_successWithIdInXml() throws Exception {
         final String output = IOUtil.toString(this.getClass().getClassLoader().getResourceAsStream("output/types.xml"));
         this.mockMvc.perform(MockMvcRequestBuilders.get("/Type").accept(MediaType.APPLICATION_XML_VALUE)
