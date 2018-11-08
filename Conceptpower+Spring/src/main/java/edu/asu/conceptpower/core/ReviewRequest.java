724c7f15109d0e60fcd823bd779ebe772ea68d71
Abhishek Kumar <abhishekkumar@gbcilsa313grad01imac.3500.dhcp.asu.edu>
2018-11-08 15:29:36 -0700

    [CCP-246] Created a new class for reviewing the request and added the getters and setters.

---
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/core/ReviewRequest.java
@@ -0,0 +1,47 @@
+package edu.asu.conceptpower.core;
+
+public class ReviewRequest {
+
+    private String comment;
+
+    enum Status {
+        Opened, Resolved, Rejected;
+    }
+
+    private String requester;
+    private String resolver;
+    private String conceptLink;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getRequester() {
+        return requester;
+    }
+
+    public void setRequester(String requester) {
+        this.requester = requester;
+    }
+
+    public String getResolver() {
+        return resolver;
+    }
+
+    public void setResolver(String resolver) {
+        this.resolver = resolver;
+    }
+
+    public String getConceptLink() {
+        return conceptLink;
+    }
+
+    public void setConceptLink(String conceptLink) {
+        this.conceptLink = conceptLink;
+    }
+
+}
