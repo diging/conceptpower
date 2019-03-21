df78550fb42ee01c14b3019a53194096afc3d55c
Abhishek Kumar <akuma168@asu.edu>
2019-03-21 12:39:51 -0700

    [newFeaure/CCP-247] Fixing indentation.

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/app/jaxb/viaf/Channel.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/app/jaxb/viaf/Channel.java
@@ -14,6 +14,7 @@ import javax.xml.bind.annotation.XmlElement;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Channel {
+    
     @XmlElement(name = "item")
     private List<Item> items;
     
