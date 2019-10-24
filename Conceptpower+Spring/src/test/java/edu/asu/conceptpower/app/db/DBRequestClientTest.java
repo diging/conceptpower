0123283a998b602fe177d82433c95cd7cd414de5
Keerthivasan Krishnamurthy <55424824+kkrish39@users.noreply.github.com>
2019-10-24 11:43:33 -0700

    [CCP-251, CCP-252, CCP-253] Test case for Update and Reopen Review Request

--- Conceptpower+Spring/src/test/java/edu/asu/conceptpower/app/db/DBRequestClientTest.java
+++ Conceptpower+Spring/src/test/java/edu/asu/conceptpower/app/db/DBRequestClientTest.java
@@ -24,6 +24,8 @@ public class DBRequestClientTest {
     
     private ReviewRequest reviewRequest = new ReviewRequest();
     
+    private ReviewRequest updateRequest = new ReviewRequest();
+    
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
@@ -35,8 +37,33 @@ public class DBRequestClientTest {
         reviewRequest.setRequester("user2");
         reviewRequest.setResolver("admin");
         reviewRequest.setStatus(ReviewStatus.OPENED);
+        
+        updateRequest.setConceptId("WID-10126926-N-05-Einstein");
+        updateRequest.setResolvingComment("Resolving Comment");
+        updateRequest.setStatus(ReviewStatus.RESOLVED);
+        updateRequest.setResolver("User3");
 
         Mockito.when(client.getReviewRequestForConcept(reviewRequest.getConceptId())).thenReturn(reviewRequest);
+        
+        ReviewRequest updatedRequest = new ReviewRequest();
+        
+        updatedRequest.setConceptId("WID-10126926-N-05-Einstein");
+        updatedRequest.setRequest("Review the Spelling");
+        updatedRequest.setRequester("user2");
+        updatedRequest.setStatus(ReviewStatus.RESOLVED);
+        updatedRequest.setResolver("User3");
+        updatedRequest.setResolvingComment("Resolving Comment");
+        
+        Mockito.when(client.updateReviewRequest(updateRequest)).thenReturn(updatedRequest);
+        
+        ReviewRequest reopenedRequest = new ReviewRequest();
+        
+        reopenedRequest.setConceptId("WID-10126926-N-05-Einstein");
+        reopenedRequest.setRequest("Review the Spelling");
+        reopenedRequest.setRequester("user2");
+        reopenedRequest.setStatus(ReviewStatus.OPENED);
+        
+        Mockito.when(client.reopenReviewRequest("WID-10126926-N-05-Einstein")).thenReturn(reopenedRequest);
     }
     @Test
     public void getReviewRequestTest() {
@@ -49,5 +76,33 @@ public class DBRequestClientTest {
         assertEquals("user2", response.getRequester());
         assertEquals("admin", response.getResolver());
         assertEquals(ReviewStatus.OPENED, response.getStatus());
+    }
+    
+    @Test
+    public void updateReviewRequestTest() {
+        ReviewRequest response  = client.updateReviewRequest(updateRequest);
+        
+        assertNotNull(response);
+        
+        assertEquals("WID-10126926-N-05-Einstein", response.getConceptId());
+        assertEquals("Review the Spelling", response.getRequest());
+        assertEquals("user2", response.getRequester());
+        assertEquals("User3", response.getResolver());
+        assertEquals(ReviewStatus.RESOLVED, response.getStatus());
+        assertEquals("Resolving Comment", response.getResolvingComment());
+    }
+    
+    @Test
+    public void reopenReviewRequestTest() {
+        ReviewRequest response = client.reopenReviewRequest("WID-10126926-N-05-Einstein");
+        
+        assertNotNull(response);
+        
+        assertEquals("WID-10126926-N-05-Einstein", response.getConceptId());
+        assertEquals("Review the Spelling", response.getRequest());
+        assertEquals("user2", response.getRequester());
+        assertEquals(null, response.getResolver());
+        assertEquals(ReviewStatus.OPENED, response.getStatus());
+        assertEquals(null, response.getResolvingComment());
     }
 }
