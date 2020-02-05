43a573c78d7997fc48ddee8831345a6f34084ef4
adhvikmadhav <56174904+adhvikmadhav@users.noreply.github.com>
2020-02-05 13:40:19 -0700

    [CCP-254] Adding concepts, change passwords for non-admin users, concepts review

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/ConceptAddController.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/ConceptAddController.java
@@ -173,7 +173,7 @@ public class ConceptAddController {
         }
         model.addAttribute("entry", conceptEntry);
         model.addAttribute("uri", uriHelper.getURI(conceptEntry));
-        return "/auth/conceptDetails";
+        return "/layouts/concepts/AddConceptReview";
     }
 
     /**
