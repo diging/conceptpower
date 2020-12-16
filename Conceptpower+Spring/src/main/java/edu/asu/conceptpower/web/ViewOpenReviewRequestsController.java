41e580bb6d2990d9bad96c671c9cd68e40cf8313
keerthivasankrishnamurthy <kkrish39@asu.edu>
2020-12-16 12:20:22 -0700

    [CCP-253] Refactored open request controller

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/ViewOpenReviewRequestsController.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/web/ViewOpenReviewRequestsController.java
@@ -17,7 +17,7 @@ import edu.asu.conceptpower.app.wrapper.ConceptEntryWrapper;
 import edu.asu.conceptpower.app.wrapper.IConceptWrapperCreator;
 
 /**
- * This endpoint renders the UI of the open review requests 
+ * This endpoint renders the UI of the open review requests
  * 
  * @author Keerthivasan Krishnamurthy
  * 
@@ -26,42 +26,41 @@ import edu.asu.conceptpower.app.wrapper.IConceptWrapperCreator;
 @Controller
 public class ViewOpenReviewRequestsController {
 	@Autowired
-    private IConceptManager conceptsManager;
-	
-    @Autowired
-    private IConceptWrapperCreator wrapperCreator;
-    
-    @Autowired
-    private IIndexService indexService;
-    
-    @Value("#{messages['INDEXER_RUNNING']}")
-    private String indexerRunning;
-    
-    @Value("#{messages['INDEXERSTATUS']}")
-    private String indexerStatus;
+	private IConceptManager conceptsManager;
 
-	@GetMapping(value="/auth/request/all/open")
-    public String viewOpenReviewRequests(ModelMap model) {
-		
+	@Autowired
+	private IConceptWrapperCreator wrapperCreator;
+
+	@Autowired
+	private IIndexService indexService;
+
+	@Value("#{messages['INDEXER_RUNNING']}")
+	private String indexerRunning;
+
+	@Value("#{messages['INDEXERSTATUS']}")
+	private String indexerStatus;
+
+	@GetMapping(value = "/auth/request/all/open")
+	public String viewOpenReviewRequests(ModelMap model) {
+		if (indexService.isIndexerRunning()) {
+			model.addAttribute("show_error_alert", true);
+			model.addAttribute("error_alert_msg", indexerRunning);
+
+			return "layouts/concepts/openrequests";
+		}
+
 		List<ConceptEntry> openReviewConcepts = conceptsManager.getAllConceptsByStatus(ReviewStatus.OPENED);
 		List<ConceptEntryWrapper> foundConcepts = null;
-		
-		if (indexService.isIndexerRunning()) {
-            model.addAttribute("show_error_alert", true);
-            model.addAttribute("error_alert_msg", indexerRunning);
-            
-            return "layouts/concepts/openrequests";
-        }
-		
-		
-        try {
-			foundConcepts = wrapperCreator.createWrappers(openReviewConcepts.toArray(new ConceptEntry[openReviewConcepts.size()]));
+
+		try {
+			foundConcepts = wrapperCreator
+					.createWrappers(openReviewConcepts.toArray(new ConceptEntry[openReviewConcepts.size()]));
 		} catch (LuceneException e) {
 			model.addAttribute(indexerStatus, e.getMessage());
 			e.printStackTrace();
 		}
-        
-        model.addAttribute("openRequests", foundConcepts);
-        return "layouts/concepts/openrequests";
-    }
+
+		model.addAttribute("openRequests", foundConcepts);
+		return "layouts/concepts/openrequests";
+	}
 }
