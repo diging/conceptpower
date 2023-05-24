5328ef564c4cdce9c7640458bdacdf5417cd6ad0
sri-9597 <srichandraallala@gmail.com>
2023-05-23 17:01:16 -0700

    [CCP-263] Upgrading spring version to 6.0.9 and updated javax.servlet dependecies to jakarta.servlet for compatibility with Java 11

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/rest/util/RestExceptionHandler.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/rest/util/RestExceptionHandler.java
@@ -1,7 +1,5 @@
 package edu.asu.conceptpower.rest.util;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +14,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.asu.conceptpower.app.exceptions.IndexerRunningException;
 import edu.asu.conceptpower.rest.msg.IConceptMessage;
 import edu.asu.conceptpower.rest.msg.IMessageRegistry;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * This class handles the exceptions from rest interface calls.
