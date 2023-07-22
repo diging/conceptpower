110c391eed875c4249ec4bf182c9983019c204a5
sri-9597 <srichandraallala@gmail.com>
2023-07-21 17:08:15 -0700

    [CCP-265] Adding login path to the requestMatchers

--- Conceptpower+Spring/src/main/java/edu/asu/conceptpower/app/config/SecurityContext.java
+++ Conceptpower+Spring/src/main/java/edu/asu/conceptpower/app/config/SecurityContext.java
@@ -5,14 +5,11 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,12 +17,13 @@ import org.springframework.security.web.access.expression.DefaultWebSecurityExpr
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 public class SecurityContext {
 
     @Autowired
@@ -35,9 +33,9 @@ public class SecurityContext {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().requestMatchers("/auth/user/**", "/auth/index/**").hasRole("CP_ADMIN")
                 .requestMatchers("/auth/**").authenticated().requestMatchers("/conceptpower/rest/concept/add")
-                .authenticated().requestMatchers("/**").permitAll().and().formLogin().loginPage("/conceptpower/login")
-                .permitAll().and().logout().logoutSuccessUrl("/login?logout").logoutUrl("/signout").permitAll()
-                .deleteCookies("JSESSIONID").and().exceptionHandling().accessDeniedPage("/forbidden");
+                .authenticated().requestMatchers("/**").permitAll().requestMatchers("/login");
+        http.formLogin().loginPage("/login").permitAll().and().logout().logoutSuccessUrl("/").logoutUrl("/signout")
+                .permitAll().deleteCookies("JSESSIONID").and().exceptionHandling().accessDeniedPage("/forbidden");
         http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher())
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler());
